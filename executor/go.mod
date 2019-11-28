2a4b41b12d0d0a180921cc572d5af2a205284bed
Marcos Barros <marcos.barros.filho@ccc.ufcg.edu.br>
2019-11-28 18:03:41 -0300

    Executor

--- executor/go.mod
+++ executor/go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/dadosjusbr/storage v0.0.0-20191121134850-f2718636de40
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c // indirect
 )
--- executor/main.go
+++ executor/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,67 +11,104 @@ import (
 	"time"
 
 	"github.com/dadosjusbr/storage"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 )
 
-func main() {
-	month := flag.Int("mes", 0, "Mês a ser analisado")
-	year := flag.Int("ano", 0, "Ano a ser analisado")
-	flag.Parse()
+type config struct {
+	OutputFolder string
+	JobList      []string
+	Month        int
+	Year         int
+	MongoURI     string
+}
 
+var c config
+
+func init() {
+	var err error
+	if err := godotenv.Load(); err != nil {
+		logError("Error loading .env file")
+		os.Exit(1)
+	}
+	if err := envconfig.Process("", &c); err != nil {
+		logError("Error loading config values from .env: %q", err.Error())
+		os.Exit(1)
+	}
+	if c.OutputFolder, err = filepath.Abs(c.OutputFolder); err != nil {
+		logError("Error trying to get absolute path for output folder: %q", err.Error())
+		os.Exit(1)
+	}
+	if err := os.Mkdir(c.OutputFolder, os.ModePerm); err != nil && !os.IsExist(err) {
+		logError("Error creating output folder(%s): %q", c.OutputFolder, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
 	commit, err := getGitCommit()
 	if err != nil {
 		logError("%s", err)
 		os.Exit(1)
 	}
 
-	jobList := []string{
-		"/home/marcosbmf/go/projects/coletores/trepb",
-	}
-	for _, job := range jobList {
-		err := build(job, commit)
+	for _, job := range c.JobList {
+		stdOut, stdErr, err := build(job, commit)
+		backup(job, "build.stdout", stdOut)
+		backup(job, "build.stderr", stdErr)
 		if err != nil {
-			logError("Building error: %s", err)
-			os.Exit(1)
+			logError("Build error %s: %q", job, err)
+			continue
 		}
-	}
-
-	outputs, execErrs := execJobs(jobList, *month, *year)
-	for _, err := range execErrs {
-		logError("%s", err)
-	}
-	result, err := json.Marshal(outputs)
-	if err != nil {
-		panic("error marshalling results")
-	}
-	fmt.Printf("%s", result)
-	if len(execErrs) > 0 {
-		os.Exit(1)
+		stdOut, stdErr, err = execDataCollector(job, c.Month, c.Year)
+		backup(job, "exec.stdout", stdOut)
+		backup(job, "exec.stderr", stdErr)
+		if err != nil {
+			logError("Execution error %s: %q", job, err)
+			continue
+		}
+		err = store(stdOut)
+		if err != nil {
+			logError("Store error %s: %q", job, err)
+			continue
+		}
 	}
 }
 
-// execJobs executes each data collector, collecting errors and outputs from the execution. Returns slice of outputs and slice of errors.
-func execJobs(jobs []string, month, year int) ([]storage.CrawlingResult, []error) {
-	var execErrors []error
-	var outputs []storage.CrawlingResult
-	for _, job := range jobs {
-		var cr storage.CrawlingResult
-		stdout, stderr, exitErr := execDataCollector(job, month, year) //stdout, stderr and exit status for data collector.
-		if statusCode(exitErr) != 0 {
-			execErrors = append(execErrors, fmt.Errorf("error executing data colector (%s): Exit status %d", job, exitErr)) // This will capture exit status.
-		}
-		if len(stderr) > 0 {
-			execErrors = append(execErrors, fmt.Errorf("error executing data colector (%s): %s", job, stderr))
-		}
-
-		err := json.Unmarshal(stdout, &cr)
-		if err != nil {
-			execErrors = append(execErrors, fmt.Errorf("error unmarshalling crawling result from (%s): %q", job, err))
-		} else {
-			outputs = append(outputs, cr)
-		}
+func store(content []byte) error {
+	storageClient, err := storage.NewClient(c.MongoURI)
+	if err != nil {
+		return fmt.Errorf("Error trying to initialize mongo client: %q", err)
+	}
+	err = storageClient.Connect()
+	if err != nil {
+		return fmt.Errorf("Error trying to connect to mongo client: %q", err)
 	}
 
-	return outputs, execErrors
+	var cr storage.CrawlingResult
+	err = json.Unmarshal(content, &cr)
+	if err != nil {
+		return fmt.Errorf("error trying to unmarshal crawling result: %q", err)
+	}
+	err = storageClient.Store(cr)
+	if err != nil {
+		return fmt.Errorf("error trying to store crawling result: %q", err)
+	}
+	err = storageClient.Disconnect()
+	if err != nil {
+		return fmt.Errorf("Error trying to disconnect from mongo client: %q", err)
+	}
+	return nil
+}
+
+// getGitCommit returns the last git commit for the local repository.
+func getGitCommit() (string, error) {
+	cmd := exec.Command("git", "rev-list", "-1", "HEAD")
+	stdout, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("getGitCommit() error: %s", err)
+	}
+	return string(stdout[:len(stdout)-1]), nil
 }
 
 // execDataCollector executes the data collector located in path and returns it's stdin, stdout and exit error if any.
@@ -80,6 +116,7 @@ func execDataCollector(path string, month, year int) ([]byte, []byte, error) {
 	cmdList := strings.Split(fmt.Sprintf("./%s --mes=%d --ano=%d", filepath.Base(path), month, year), " ")
 	cmd := exec.Command(cmdList[0], cmdList[1:]...)
 	cmd.Dir = path
+	cmd.Env = append(cmd.Env, fmt.Sprintf("OUTPUT_FOLDER=%s/%s", c.OutputFolder, filepath.Base(path)))
 	var outb, errb bytes.Buffer
 	cmd.Stdout = &outb
 	cmd.Stderr = &errb
@@ -87,31 +124,15 @@ func execDataCollector(path string, month, year int) ([]byte, []byte, error) {
 	return outb.Bytes(), errb.Bytes(), err
 }
 
-// getGitCommit returns the last git commit for the local repository.
-func getGitCommit() (string, error) {
-	cmd := exec.Command("git", "rev-list", "-1", "HEAD")
-	stdin, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", fmt.Errorf("getGitCommit() error: %s", err)
-	}
-	return string(stdin[:len(stdin)-1]), nil
-}
-
 // build runs a go build for each path. It will also insert the value of main.gitCommit in the binaries.
-func build(path, commit string) error {
-	cmd := exec.Command("go", "build", "-ldflags", fmt.Sprintf(`"-X main.gitCommit=%s"`, commit))
-	cmd.Dir = fmt.Sprintf(path)
+func build(path, commit string) ([]byte, []byte, error) {
+	cmd := exec.Command("go", "build", "-ldflags", fmt.Sprintf("-X main.gitCommit=%s", commit))
+	cmd.Dir = path
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
 	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("build() error (%s): %s", path, err)
-	}
-	return nil
-}
-
-// fatalError prints to Stderr
-func logError(format string, args ...interface{}) {
-	time := fmt.Sprintf("%s: ", time.Now().Format(time.RFC3339))
-	fmt.Fprintf(os.Stderr, time+format+"\n", args...)
+	return outb.Bytes(), errb.Bytes(), err
 }
 
 // statusCode returns the exit code returned for the cmd execution.
@@ -126,4 +147,31 @@ func statusCode(err error) int {
 		return exitError.ExitCode()
 	}
 	return -2
+}
+
+// fatalError prints to Stderr
+func logError(format string, args ...interface{}) {
+	time := fmt.Sprintf("%s: ", time.Now().Format(time.RFC3339))
+	fmt.Fprintf(os.Stderr, time+format+"\n", args...)
+}
+
+// backup will log content of job-description when content is not empty
+func backup(job string, desc string, content []byte) {
+	if len(content) == 0 {
+		return
+	}
+	path := fmt.Sprintf("%s/%s(%d-%d)-%s-%s", c.OutputFolder, filepath.Base(job), c.Month, c.Year, desc, time.Now().Format(time.RFC3339))
+	fmt.Println(path, c.OutputFolder)
+	f, err := os.Create(path)
+	if err != nil {
+		logError("backup error: error creating file: %s", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	_, err = f.Write(content)
+	if err != nil {
+		logError("backup error: error writing to file: %s", err)
+		os.Exit(1)
+	}
 }
