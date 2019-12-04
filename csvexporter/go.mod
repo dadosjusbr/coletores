b0209d0e8ede11d3ebaa4cfb58c7bc3b0ee73161
Daniel Fireman <danielfireman@gmail.com>
2019-12-04 12:31:17 -0300

    Primeiro take no exporter

---
+++ csvexporter/go.mod
@@ -0,0 +1,11 @@
+module csvexporter
+
+go 1.12
+
+require (
+	github.com/dadosjusbr/storage v0.0.0-20191202174801-ad5aae8c5e2f // indirect
+	github.com/kelseyhightower/envconfig v1.4.0
+	go.mongodb.org/mongo-driver v1.1.3
+	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
---
+++ csvexporter/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/dadosjusbr/storage"
+	"github.com/kelseyhightower/envconfig"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type config struct {
+	MongoDBURI        string `envconfig:"MONGODB_URI" required:"true"`
+	MongoDBName       string `envconfig:"MONGODB_NAME" required:"true"`
+	MongoDBCollection string `envconfig:"MONGODB_COLLECTION" required:"true"`
+}
+
+func main() {
+	var c config
+	if err := envconfig.Process("", &c); err != nil {
+		log.Fatalf("error loading config values: %q\n", err.Error())
+	}
+	fmt.Fprintf(os.Stderr, "Loaded prefs:%+v\n", c)
+	mgo, err := mongo.NewClient(options.Client().ApplyURI(c.MongoDBURI))
+	if err != nil {
+		log.Fatalf("error creating mongodb client: %q\n", err.Error())
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	if err = mgo.Connect(ctx); err != nil {
+		log.Fatalf("error connecting to mongodb: %q\n", err.Error())
+	}
+	// Calling Connect does not block for server discovery. Need to ping.
+	ctx, _ = context.WithTimeout(context.Background(), 30*time.Second)
+	if err = mgo.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatalf("could not find any available mongo instance: %q\n", err.Error())
+	}
+	fmt.Fprintf(os.Stderr, "Sucessfully connected to mongodb %s\n", c.MongoDBURI)
+
+	cur, err := mgo.Database(c.MongoDBName).Collection(c.MongoDBCollection).Find(context.Background(), bson.M{})
+	if err != nil {
+		log.Fatalf("error querying mongodb:%q", err)
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		var mi storage.AgencyMonthlyInfo
+		if err := cur.Decode(&mi); err != nil {
+			log.Fatalf("error decoding value from mongodb:%q\n", err)
+		}
+		fmt.Println(mi)
+		break
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatalf("error dealing with mongodb cursor:%q\n", err)
+	}
+}
