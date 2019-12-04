49f0717dfeb3576a983aaf738afd361e1c9e2c57
Marcos Barros <marcos.barros.filho@ccc.ufcg.edu.br>
2019-12-04 18:23:37 -0300

    Checagem final dos headers das tabelas e obtençao dos funcionarios em array de strings

--- mppb/parser.go
+++ mppb/parser.go
@@ -14,40 +14,100 @@ const (
 	REMUNERACOES = 2
 )
 
-var mapping = map[string]int{
-	"MATRÍCULA":             0,
-	"NOME":                  1,
-	"CARGO":                 2,
-	"LOTAÇÃO":               3,
-	"CARGO EFETIVO":         4,
-	"OUTRAS VERBAS":         5,
-	"CARGO EM COMISSÃO":     6,
-	"GRATIFICAÇÃO NATALINA": 7,
-	"FÉRIAS":                8,
-	"PERMANÊNCIA":           9,
-	"TEMPORÁRIAS":           10,
-	"INDENIZATÓRIAS":        11,
-	"PREVIDENCIÁRIA":        13,
-	"IMPOSTO":               14,
-	"RETENÇÃO":              15,
+var headersMap = []map[string]int{
+	ESTAGIARIOS: map[string]int{
+		"NOME":                0,
+		"CARGO":               1,
+		"LOTAÇÃO":             2,
+		"REMUNERAÇÃO":         3,
+		"OUTRAS VERBAS":       4,
+		"FUNÇÃO DE CONFIANÇA": 5,
+		"13º VENCIMENTO":      6,
+		"FÉRIAS":              7,
+		"PERMANÊNCIA":         8,
+		"PREVIDENCIÁRIA":      10,
+		"IMPOSTO":             11,
+		"RETENÇÃO":            12,
+		"TEMPORÁRIAS":         16,
+		"INDENIZAÇÕES":        15,
+	},
+
+	INDENIZACOES: map[string]int{
+		"MATRÍCULA":      0,
+		"ALIMENTAÇÃO":    4,
+		"SAÚDE":          5,
+		"PECÚNIA":        6,
+		"MORADIA":        7,
+		"NATALIDADE":     8,
+		"AJUDA DE CUSTO": 9, // first col for eventualBenefits
+		"DESPESA":        21,
+	},
+
+	REMUNERACOES: map[string]int{
+		"MATRÍCULA":             0,
+		"NOME":                  1,
+		"CARGO":                 2,
+		"LOTAÇÃO":               3,
+		"CARGO EFETIVO":         4,
+		"OUTRAS VERBAS":         5,
+		"CARGO EM COMISSÃO":     6,
+		"GRATIFICAÇÃO NATALINA": 7,
+		"FÉRIAS":                8,
+		"PERMANÊNCIA":           9,
+		"TEMPORÁRIAS":           10,
+		"INDENIZATÓRIAS":        11,
+		"PREVIDENCIÁRIA":        13,
+		"IMPOSTO":               14,
+		"RETENÇÃO":              15,
+	},
 }
 
 // Parser parses the ods tables.
 func Parser(files []string) error {
+	data, err := dataAsSlices(files)
+	if err != nil {
+		return fmt.Errorf("error trying to parse data as slices: %q", err)
+	}
+	fmt.Printf("%v\n", data)
+	return nil
+}
+
+func dataAsSlices(files []string) (map[int][][]string, error) {
+	var (
+		perksAndbenefits [][]string
+		employees        [][]string
+		interns          [][]string
+	)
 	for _, file := range files {
 		var doc ods.Doc
 		f, err := ods.Open(file)
 		if err != nil {
-			return fmt.Errorf("ods.Open error(%s): %q", file, err)
+			return nil, fmt.Errorf("ods.Open error(%s): %q", file, err)
 		}
 		f.ParseContent(&doc)
 		fileType := dataType(file)
 		if err := assertHeaders(doc, fileType); err != nil {
 			logError("assertHeaders() for %s error: %q", file, err)
+			continue
+		}
+		switch fileType {
+		case REMUNERACOES:
+			employees = append(employees, getEmployees(doc)...)
+			break
+		case ESTAGIARIOS:
+			interns = append(interns, getEmployees(doc)...)
+			break
+		case INDENIZACOES:
+			perksAndbenefits = append(perksAndbenefits, getEmployees(doc)...)
+			break
 		}
 		f.Close()
 	}
-	return nil
+	return map[int][][]string{
+		REMUNERACOES: employees,
+		ESTAGIARIOS:  interns,
+		INDENIZACOES: perksAndbenefits,
+	}, nil
 }
 
 func dataType(fileName string) int {
@@ -55,20 +115,32 @@ func dataType(fileName string) int {
 		return INDENIZACOES
 	} else if strings.Contains(fileName, "estagiarios") {
 		return ESTAGIARIOS
-	} else if strings.Contains(fileName, "membros") || strings.Contains(fileName, "servidores") {
+	} else if strings.Contains(fileName, "membros") || strings.Contains(fileName, "servidores") || strings.Contains(fileName, "aposentados") {
 		return REMUNERACOES
 	}
 	return -1
 }
 
-func getHeaders(doc ods.Doc, fileType int) []string {
-	var headers []string
-	raw := doc.Table[0].Strings()[5:8]
-	for row := range raw {
-		for col := range raw[row] {
-			raw[row][col] = strings.ToUpper(strings.ReplaceAll(raw[row][col], "\n", " "))
+func getEmployees(doc ods.Doc) [][]string {
+	var lastLine int
+	for i, values := range doc.Table[0].Strings() {
+		if len(values) < 1 {
+			continue
+		}
+		if values[0] == "TOTAL GERAL" {
+			lastLine = i - 1
+			break
 		}
 	}
+	if lastLine == 0 {
+		return [][]string{}
+	}
+	return cleanStrings(doc.Table[0].Strings()[10:lastLine])
+}
+
+func getHeaders(doc ods.Doc, fileType int) []string {
+	var headers []string
+	raw := cleanStrings(doc.Table[0].Strings()[5:8])
 	switch fileType {
 	case INDENIZACOES:
 		headers = append(headers, raw[0][:4]...)
@@ -94,27 +166,12 @@ func getHeaders(doc ods.Doc, fileType int) []string {
 
 func assertHeaders(doc ods.Doc, fileType int) error {
 	headers := getHeaders(doc, fileType)
-	switch fileType {
-	case REMUNERACOES:
-		for key, value := range mapping {
-			if err := containsHeader(headers, key, value); err != nil {
-				return err
-			}
+	for key, value := range headersMap[fileType] {
+		if err := containsHeader(headers, key, value); err != nil {
+			return err
 		}
-		return nil
-	case ESTAGIARIOS:
-		for key, value := range mapping {
-			if key == "MATRÍCULA" {
-				continue
-			}
-			if err := containsHeader(headers, key, value-1); err != nil {
-				return err
-			}
-		}
-		return nil
-	default:
-		return fmt.Errorf("couldn't fit fileType: %d", fileType)
 	}
+	return nil
 }
 
 func containsHeader(headers []string, key string, value int) error {
@@ -122,4 +179,13 @@ func containsHeader(headers []string, key string, value int) error {
 		return nil
 	}
 	return fmt.Errorf("couldn't find %s at position %d", key, value)
+}
+
+func cleanStrings(raw [][]string) [][]string {
+	for row := range raw {
+		for col := range raw[row] {
+			raw[row][col] = strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(raw[row][col], "N/D", ""), "\n", " "))
+		}
+	}
+	return raw
 }
