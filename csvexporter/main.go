692422d42856f22189d0f3899c89fe742ebbfd3c
Marcos Barros <marcos.barros.filho@ccc.ufcg.edu.br>
2019-12-05 16:04:02 -0300

    Revert "Traduçao das estruturas para csv"
    This reverts commit c5dc3bb829f7593d938d0bdc5c8b94d23b35da8b.

--- csvexporter/main.go
+++ csvexporter/main.go
@@ -31,115 +31,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("error creating mongodb client: %q\n", err.Error())
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	if err = mgo.Connect(ctx); err != nil {
 		log.Fatalf("error connecting to mongodb: %q\n", err.Error())
 	}
-	defer cancel()
 	// Calling Connect does not block for server discovery. Need to ping.
-	ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, _ = context.WithTimeout(context.Background(), 30*time.Second)
 	if err = mgo.Ping(ctx, readpref.Primary()); err != nil {
 		log.Fatalf("could not find any available mongo instance: %q\n", err.Error())
 	}
 	fmt.Fprintf(os.Stderr, "Sucessfully connected to mongodb %s\n", c.MongoDBURI)
-	defer cancel()
 
 	cur, err := mgo.Database(c.MongoDBName).Collection(c.MongoDBCollection).Find(context.Background(), bson.M{})
 	if err != nil {
 		log.Fatalf("error querying mongodb:%q", err)
 	}
 	defer cur.Close(context.Background())
-	csvHeaders()
 	for cur.Next(context.Background()) {
 		var mi storage.AgencyMonthlyInfo
 		if err := cur.Decode(&mi); err != nil {
 			log.Fatalf("error decoding value from mongodb:%q\n", err)
 		}
-		err = logAgencies(mi)
-		if err != nil {
-			log.Fatalf("error logging agency: %q", err)
-		}
+		// TODO: Para cada registro, transformar em CSV e imprimir na saída padrão.
 	}
 	if err := cur.Err(); err != nil {
 		log.Fatalf("error dealing with mongodb cursor:%q\n", err)
 	}
-}
-
-func csvHeaders() {
-	fmt.Printf(`"aid", "year", "month",`)
-	fmt.Printf(`"reg", "name", "role", "type", "workplace", "active",`)
-	fmt.Printf(`"income_total", "wage",`)
-	fmt.Printf(`"perks_total", "perks_food", "perks_transportation", "perks_preschool", "perks_health","perks_birthaid","perks_housingaid","perks_subsistence", "perks_others",`)
-	fmt.Printf(`"others_total", "others_personalbenefits", "others_eventualbenefits", "others_positionoftrust", "others_daily", "others_gratification", "others_originposition", "others_others",`)
-	fmt.Printf(`"discounts_total", "discounts_prevcontribution", "discounts_ceilretention", "discounts_incometax", "discounts_others"`)
-	fmt.Println()
-}
-
-func logAgencies(ag storage.AgencyMonthlyInfo) error {
-	for _, e := range ag.Employee {
-		basicInfo := fmt.Sprintf("%q, %d, %d,", ag.AgencyID, ag.Year, ag.Month)
-		empInfo := empInfo(e)
-		fmt.Println(basicInfo + empInfo[:len(empInfo)-1])
-	}
-	return nil
-}
-
-func empInfo(e storage.Employee) string {
-	basicInfo := fmt.Sprintf("%q, %q, %q, %q, %q, %t,", e.Reg, e.Name, e.Role, e.Type, e.Workplace, e.Active)
-	income := incomeInfo(e.Income)
-	discounts := discountInfo(e.Discounts)
-	line := basicInfo + income + discounts
-	return line
-}
-
-func incomeInfo(i *storage.IncomeDetails) string {
-	if i == nil {
-		return ",,,,,,,,,,,,,,,,,,,,,"
-	}
-	result := fmt.Sprintf("%.2f,", i.Total) + getFloatValues(i.Wage)
-	// Perks
-	if i.Perks == nil {
-		result += ",,,,,,,,,,"
-	} else {
-		result += fmt.Sprintf("%.2f,", i.Perks.Total) +
-			getFloatValues(i.Perks.Food, i.Perks.Transportation, i.Perks.PreSchool, i.Perks.Health, i.Perks.BirthAid, i.Perks.HousingAid, i.Perks.Subsistence) +
-			getMapTotal(i.Perks.Others)
-	}
-	// Others
-	if i.Other == nil {
-		result += ",,,,,,,,,"
-	} else {
-		result += fmt.Sprintf("%.2f,", i.Other.Total) +
-			getFloatValues(i.Other.PersonalBenefits, i.Other.EventualBenefits, i.Other.PositionOfTrust, i.Other.Daily, i.Other.Gratification, i.Other.OriginPosition) +
-			getMapTotal(i.Other.Others)
-	}
-	return result
-}
-
-func discountInfo(d *storage.Discount) string {
-	if d == nil {
-		return ",,,,,"
-	}
-	result := fmt.Sprintf("%.2f,", d.Total) + getFloatValues(d.PrevContribution, d.CeilRetention, d.IncomeTax) + getMapTotal(d.Others)
-	return result
-}
-
-func getFloatValues(floats ...*float64) string {
-	result := ""
-	for _, p := range floats {
-		if p == nil {
-			result += ","
-		} else {
-			result += fmt.Sprintf("%.2f,", *p)
-		}
-	}
-	return result
-}
-
-func getMapTotal(m map[string]float64) string {
-	total := 0.
-	for _, v := range m {
-		total += v
-	}
-	return fmt.Sprintf("%.2f,", total)
 }
