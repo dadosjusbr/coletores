fefcebdcd8ea4b6516c76bfd57c85d2f818a4be5
abuarquemf <abuarquemf@gmail.com>
2020-03-14 16:32:30 -0300

    added test for active members

--- mppe/crawler_test.go
+++ mppe/crawler_test.go
@@ -4,19 +4,27 @@ import (
 	"testing"
 )
 
+var flagtests = []struct {
+	name            string
+	memberType      string
+	patternToSearch string
+	fakeHTMLFile    string
+	desiredOutput   string
+}{
+	{"Should get right code for membros ativos for years differente of 2017", "membrosAtivos", ":membros-ativos-02-2019", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "5051"},
+	{"Should fail gettting code for membros ativos for years differente of 2017", "membrosAtivos", ":membros-ativos-fevereiro-2019", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "nil"},
+	{"Should fail gettting code for membros ativos for year 2017", "membrosAtivos", ":membros-ativos-fevereiro-2017", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "nil"},
+	{"Should get right code for membros ativos for year 2017", "membrosAtivos", ":quadro-de-membros-ativos-fevereiro-2017", ".../4554/resource-fevereiro:download=5051:quadro-de-membros-ativos-fevereiro-2017", "5051"},
+}
+
 func TestFindFileIdentifier(t *testing.T) {
-	fakeHTMLFile := ".../4554/someResourcePath-fevereiro:download=5051:membros-ativos-02-2019"
-	category := "remuneracao-de-todos-os-membros-ativos"
-	memberType := "membrosAtivos"
-	year := 2019
-	month := 2
-	patternToSearch := pathResolver(month, year, category)
-	code, err := findFileIdentifier(memberType, fakeHTMLFile, patternToSearch)
-	if code != "5051" {
-		t.Error()
-	}
-	if err != nil {
-		t.Error()
+	for _, tt := range flagtests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _ := findFileIdentifier(tt.memberType, tt.fakeHTMLFile, tt.patternToSearch)
+			if code != tt.desiredOutput {
+				t.Errorf("got %s, want %s", code, tt.desiredOutput)
+			}
+		})
 	}
 }
 
