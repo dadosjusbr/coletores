f7dd741be9816d70b2cb0019b9af4572a46e7eb2
abuarquemf <abuarquemf@gmail.com>
2020-03-14 17:06:47 -0300

    added test for inactive members

--- mppe/crawler_test.go
+++ mppe/crawler_test.go
@@ -12,9 +12,12 @@ var flagtests = []struct {
 	desiredOutput   string
 }{
 	{"Should get right code for membros ativos for years differente of 2017", "membrosAtivos", ":membros-ativos-02-2019", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "5051"},
-	{"Should fail gettting code for membros ativos for years differente of 2017", "membrosAtivos", ":membros-ativos-fevereiro-2019", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "nil"},
-	{"Should fail gettting code for membros ativos for year 2017", "membrosAtivos", ":membros-ativos-fevereiro-2017", ".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019", "nil"},
-	{"Should get right code for membros ativos for year 2017", "membrosAtivos", ":quadro-de-membros-ativos-fevereiro-2017", ".../4554/resource-fevereiro:download=5051:quadro-de-membros-ativos-fevereiro-2017", "5051"},
+	{"Should fail gettting code for membros ativos for years differente of 2017", "membrosAtivos", ":membros-ativos-fevereiro-2019", ".../4554/status:download=5051:membros-ativos-02-2019", "nil"},
+	{"Should fail gettting code for membros ativos for year 2017", "membrosAtivos", ":membros-ativos-fevereiro-2017", ".../4554/recursos:download=5051:membros-ativos-02-2019", "nil"},
+	{"Should get right code for membros ativos for year 2017", "membrosAtivos", ":quadro-de-membros-ativos-fevereiro-2017", ".../4554/resource-online:download=5051:quadro-de-membros-ativos-fevereiro-2017", "5051"},
+	{"Should get right code for membros in ativos for year differente of 2014", "membrosInativos", ":membros-inativos-03-2017", ".../4554/resource:download=4312:membros-inativos-03-2017", "4312"},
+	{"Should get right code for membros in ativos for year 2014 and month different of janeiro", "membrosInativos", ":membros-inativos-03-2014", ".../4554/resource:download=1234:membros-inativos-03-2014", "1234"},
+	{"Should get right code for membros in ativos for year 2014 and month janeiro", "membrosInativos", ":membros-inativos-01-2015", ".../4554/resource:download=4567:membros-inativos-01-2015", "4567"},
 }
 
 func TestFindFileIdentifier(t *testing.T) {
