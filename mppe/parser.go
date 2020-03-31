e2746df1db76b696fae260ce03fc644d1bd8e030
abuarquemf <abuarquemf@gmail.com>
2020-03-30 23:21:52 -0300

    added function to check if a document is active or not

--- mppe/parser.go
+++ mppe/parser.go
@@ -1,5 +1,23 @@
 package main
 
+// it checks if the document is of active members or not
+func isActive(documentIdentification string) bool {
+	switch documentIdentification {
+	case "proventos-de-todos-os-membros-inativos":
+		return false
+	case "proventos-de-todos-os-servidores-inativos":
+		return false
+	case "verbas-referentes-a-exercicios-anteriores":
+		return false
+	case "verbas-indenizatorias-e-outras-remuneracoes-temporarias":
+		return false
+	case "valores-percebidos-por-todos-os-pensionistas":
+		return false
+	default:
+		return true
+	}
+}
+
 // it cuts off month, year and extension from name
 // to get file name
 func getFileDocumentation(fileName string) string {
--- mppe/parser_test.go
+++ mppe/parser_test.go
@@ -2,6 +2,31 @@ package main
 
 import "testing"
 
+func TestIsActive(t *testing.T) {
+	testCases := []struct {
+		name string
+		in   string
+		out  bool
+	}{
+		{"It should give false", "proventos-de-todos-os-membros-inativos", false},
+		{"It should give false", "proventos-de-todos-os-servidores-inativos", false},
+		{"It should give true", "remuneracao-de-todos-os-membros-ativos", true},
+		{"It should give true", "remuneracao-de-todos-os-servidores-atuvos", true},
+		{"It should give true", "valores-percebidos-por-todos-os-colaboradores", true},
+		{"It should give false", "valores-percebidos-por-todos-os-pensionistas", false},
+		{"It should give false", "verbas-indenizatorias-e-outras-remuneracoes-temporarias", false},
+		{"It should give false", "verbas-referentes-a-exercicios-anteriores", false},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			res := isActive(tt.in)
+			if res != tt.out {
+				t.Errorf("got %t, want %t", res, tt.out)
+			}
+		})
+	}
+}
+
 func TestGetTypeOfFile(t *testing.T) {
 	testCases := []struct {
 		name string
