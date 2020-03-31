ad40f1d71e04dd953b0054d49de31268278479b4
abuarquemf <abuarquemf@gmail.com>
2020-03-31 12:37:04 -0300

    added function to get employee type

--- mppe/parser.go
+++ mppe/parser.go
@@ -1,5 +1,25 @@
 package main
 
+// it returns the employee type
+func getType(documentIdentification string) string {
+	switch documentIdentification {
+	case "proventos-de-todos-os-membros-inativos":
+		return "membro"
+	case "proventos-de-todos-os-servidores-inativos":
+		return "servidor"
+	case "remuneracao-de-todos-os-membros-ativos":
+		return "membro"
+	case "remuneracao-de-todos-os-servidores-atuvos":
+		return "servidor"
+	case "valores-percebidos-por-todos-os-colaboradores":
+		return "colaborador"
+	case "valores-percebidos-por-todos-os-pensionistas":
+		return "pensionista"
+	default:
+		return "indefinido"
+	}
+}
+
 // it checks if the document is of active members or not
 func isActive(documentIdentification string) bool {
 	switch documentIdentification {
--- mppe/parser_test.go
+++ mppe/parser_test.go
@@ -2,6 +2,31 @@ package main
 
 import "testing"
 
+func TestGetType(t *testing.T) {
+	testCases := []struct {
+		name string
+		in   string
+		out  string
+	}{
+		{"It should give membro", "proventos-de-todos-os-membros-inativos", "membro"},
+		{"It should give servidor", "proventos-de-todos-os-servidores-inativos", "servidor"},
+		{"It should give membro", "remuneracao-de-todos-os-membros-ativos", "membro"},
+		{"It should give servidor", "remuneracao-de-todos-os-servidores-atuvos", "servidor"},
+		{"It should give colaborador", "valores-percebidos-por-todos-os-colaboradores", "colaborador"},
+		{"It should give pensionista", "valores-percebidos-por-todos-os-pensionistas", "pensionista"},
+		{"It should give indefinido", "verbas-indenizatorias-e-outras-remuneracoes-temporarias", "indefinido"},
+		{"It should give indefinido", "verbas-referentes-a-exercicios-anteriores", "indefinido"},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			res := getType(tt.in)
+			if res != tt.out {
+				t.Errorf("got %s, want %s", res, tt.out)
+			}
+		})
+	}
+}
+
 func TestIsActive(t *testing.T) {
 	testCases := []struct {
 		name string
