a58842d2e383f7b17868ebd48384d91726ef5377
abuarquemf <abuarquemf@gmail.com>
2020-03-31 12:50:42 -0300

    added parser implementation

--- mppe/parser.go
+++ mppe/parser.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/dadosjusbr/storage"
+)
+
+const (
+	// page name to process
+	sheetName = "Sheet"
+
+	// it is the column of unique code indentifier
+	registerCodeColumm = "A"
+
+	// it is the column of employee name
+	nameColumn = "B"
+
+	// it is the column of roles
+	roleColumn = "C"
+)
+
+// Parse parses the xlsx tables
+func Parse(paths []string) ([]storage.Employee, error) {
+	var employees []storage.Employee
+	for _, path := range paths {
+		documentIdentification := getFileDocumentation(path)
+		file, err := excelize.OpenFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("error opening document %s for parse: %q", documentIdentification, err)
+		}
+		rows := file.GetRows(sheetName)
+		var employee storage.Employee
+		for index, row := range rows {
+			if index == 1 || index == 2 || index == 3 {
+				continue
+			}
+			employee = storage.Employee{
+				Reg:       file.GetCellValue(sheetName, fmt.Sprintf("%s%d", registerCodeColumm, index)), //getRegisterCode(file),
+				Name:      file.GetCellValue(sheetName, fmt.Sprintf("%s%d", nameColumn, index)),         //getName(file),
+				Role:      file.GetCellValue(sheetName, fmt.Sprintf("%s%d", roleColumn, index)),         //getRole(file),
+				Type:      getType(documentIdentification),
+				Workplace: "mppe",
+				Active:    isActive(documentIdentification),
+				Income:    getIncome(row),
+				Discounts: getDiscounts(row),
+			}
+		}
+		employees = append(employees, employee)
+	}
+	return employees, nil
+}
+
+func getDiscounts(row []string) *storage.Discount {
+	return nil
+}
+
+func getIncome(row []string) *storage.IncomeDetails {
+	return nil
+}
+
 // it returns the employee type
 func getType(documentIdentification string) string {
 	switch documentIdentification {
