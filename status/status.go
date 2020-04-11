a7ab28fb80e94d507a05c8e88f8cfd769ba9f839
abuarquemf <abuarquemf@gmail.com>
2020-04-10 22:55:02 -0300

    fixed implementation to StatusError implements error

--- status/status.go
+++ status/status.go
@@ -1,6 +1,8 @@
 package status
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 )
@@ -8,63 +10,38 @@ import (
 // Code is a custom type to represent ints
 type Code int
 
-// Colector status codes. They're sorted in a semantic order
-// where in the house of 100's are the possible erros for the
-// setup process, at the 200's the possible erros on Crawling,
-// at 300's the ones possible on Parsing and at 400's the general
-// errors.
 const (
 	// OK means that the process worked without errors
 	OK Code = 0
 
-	// MonthAndYearNotProvided means that month and year to search were not provided
-	MonthAndYearNotProvided Code = 100
+	// InvalidParameters should be used for scenarios where month and year are not valid
+	InvalidParameters Code = 1
 
-	// InvalidMonth should be used if the colector has a month that it does not suport yet
-	InvalidMonth Code = 101
+	// SystemError should be used for scenarios like i/o erros, for example a failure on opening a file
+	SystemError Code = 2
 
-	// InvalidYear should be used if the colector has a year that it does not suport yet
-	InvalidYear Code = 102
-
-	// CouldNotCreateDirectory means that the directory to hold files could not be created
-	CouldNotCreateDirectory Code = 103
-
-	// ServiceUnavailable means that the website is unavailable
-	ServiceUnavailable Code = 200
-
-	// RequestTimeout means that the request time timedout
-	RequestTimeout Code = 201
+	// ConnectionError should be used for scenarios with connection problems, like timeouts or service unavailable
+	ConnectionError Code = 3
 
 	// DataUnavailable means that the desired data was not found on crawling
-	DataUnavailable Code = 202
+	DataUnavailable Code = 4
 
-	// CouldNotOpenFile means that an error has occurred trying to read the file
-	CouldNotOpenFile Code = 300
+	//InvalidFile should be used for invalid files or for scenarios where some data could not be extracted
+	InvalidFile Code = 5
 
-	// CouldNotExtractData means that ocurred and error extracting a value from file
-	CouldNotExtractData Code = 301
-
-	// Unexpected means that something no expected has happend
-	Unexpected Code = 400
+	// Unknown means that something unexpected has happend
+	Unknown Code = 6
 )
 
 var (
 	statusText = map[Code]string{
-		OK: "OK",
-
-		MonthAndYearNotProvided: "Month and year not provided",
-		InvalidMonth:            "Invalid Month",
-		InvalidYear:             "Invalid Year",
-		CouldNotCreateDirectory: "Could not create directory",
-
-		ServiceUnavailable: "Service Unavailable",
-		RequestTimeout:     "Request Timedout",
-		DataUnavailable:    "Data Unavailable",
-
-		CouldNotOpenFile:    "Could not open file",
-		CouldNotExtractData: "Could not extract data",
-
-		Unexpected: "Unexpected",
+		OK:                "OK",
+		InvalidParameters: "Invalid Parameters",
+		SystemError:       "System Error",
+		ConnectionError:   "Connection Error",
+		DataUnavailable:   "Data Unavailable",
+		InvalidFile:       "Invalid File",
+		Unknown:           "Unknown",
 	}
 )
 
@@ -74,9 +51,13 @@ func Text(code Code) string {
 	return statusText[code]
 }
 
-// ExitFromError exits the program logging a message an
-// setting the process code
-func ExitFromError(statusError *StatusError) {
-	log.Println(statusError.Message)
-	os.Exit(int(statusError.Code))
+// ExitFromError logs the error message and call os.Exit
+// passing the code if err is of type StatusError
+func ExitFromError(err error) {
+	log.Println(fmt.Errorf("%q", err))
+	var se *StatusError
+	if errors.As(err, &se) {
+		os.Exit(int(se.Code))
+	}
+	os.Exit(int(Unknown))
 }
