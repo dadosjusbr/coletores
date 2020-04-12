f54b68ec695276baa56b7cc1465e564f20282713
Daniel Fireman <danielfireman@gmail.com>
2020-04-12 10:12:52 -0300

    Resolvendo erro de linter: 'status/status_error.go:6:6: type name will be used as status.StatusError by other packages, and that stutters; consider calling this Error'

--- status/error.go
+++ status/error.go
@@ -3,19 +3,19 @@ package status
 import "fmt"
 
 // StatusError wraps a code and a error messager
-type StatusError struct {
+type Error struct {
 	Err  error
 	Code Code
 }
 
-// NewStatusError creates a new StatusError
-func NewStatusError(code Code, error error) error {
-	return &StatusError{
+// NewError creates a new StatusError
+func NewError(code Code, error error) error {
+	return &Error{
 		Err:  error,
 		Code: code,
 	}
 }
 
-func (se *StatusError) Error() string {
+func (se *Error) Error() string {
 	return fmt.Sprintf("error %v , %v", se.Code, se.Err.Error())
 }
