d13362fa00a7e6036ca7bb08a049e4ba1b002c4b
abuarquemf <abuarquemf@gmail.com>
2020-05-01 16:25:07 -0300

    removed unused function

--- mppe/main.go
+++ mppe/main.go
@@ -69,8 +69,3 @@ func main() {
 	}
 	fmt.Printf("%s", string(crJSON))
 }
-
-func logError(format string, args ...interface{}) {
-	time := fmt.Sprintf("%s: ", time.Now().Format(time.RFC3339))
-	fmt.Fprintf(os.Stderr, time+format+"\n", args...)
-}
