0b5b1b4412b19f8652cadcf66327429308b43ebe
abuarquemf <abuarquemf@gmail.com>
2020-05-05 17:19:31 -0300

    safet commit

--- mppe/integration_test.go
+++ mppe/integration_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/dadosjusbr/storage"
 )
 
+//proventos-de-todos-os-servidores-inativos-02-2019.xlsx
 func TestIntegration(t *testing.T) {
 	testCases := []struct {
 		name     string
@@ -19,41 +20,39 @@ func TestIntegration(t *testing.T) {
 		server   *httptest.Server
 		employee storage.Employee
 	}{
-		{"Test for proventos-de-todos-os-membros-inativos", 2, 2019,
+		{"Test for proventos-de-todos-os-servidores-inativos-02-2019", 2, 2019,
 			httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				// it was call to download the file
-				if strings.Contains(r.URL.RequestURI(), "download") {
-					path := "./test_files/proventos-de-todos-os-membros-inativos.xlsx"
-					b, err := ioutil.ReadFile(path)
-					if err != nil {
-						t.Errorf("expecting null err, got %q", err)
-					}
-					w.Write(b)
-					return
-				}
-				// it was call to make crawl
-				fmt.Fprint(w, "<div><link src=\".../4554/resource-fevereiro:download=5051:membros-ativos-02-2019\"/></div>"+
-					"<div><link src=\".../4554/resource:download=4312:membros-inativos-02-2019\"/></div>"+
-					"<div><link src=\".../31342sas2/endpoint:download=9999:servidores-ativos-02-2019\"/></div>"+
-					"<div><link src=\".../ghytr6/resource:download=1098:servidores-inativos-02-2019\"/></div>"+
-					"<div><link src=\".../5tghjuw2/Controller:random=5453:pensionistas-02-2019\"/></div>"+
-					"<div><link src=\".../random/servlet:code=3490:contracheque-valores-percebidos-colaboradores-fevereiro\"/></div>"+
-					"<div><link src=\".../controller_servlet:download=5378:dea-022019\"/></div>"+
-					"<div><link src=\".../members_controller:code=8712:virt-fevereiro-2019\"/></div>")
-				return
-			})), storage.Employee{
-				Reg:       "1885065",
-				Name:      "ADLLA RIJO FARIAS COSTA",
-				Active:    false,
-				Type:      "membro",
-				Workplace: "mppe",
-				Role:      "PROMOTOR 1. ENTRANCIA                             ",
-			}},
-		{"Test for proventos-de-todos-os-servidores-inativos", 2, 2019,
-			httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				// it was call to download the file
-				if strings.Contains(r.URL.RequestURI(), "download") {
-					path := "./test_files/proventos-de-todos-os-servidores-inativos.xlsx"
+				uri := r.URL.RequestURI()
+				if strings.Contains(uri, "download") {
+					// if strings.Contains(uri, "proventos-de-todos-os-membros-inativos") {
+					// 	path := "./output/proventos-de-todos-os-membros-inativos-02-2019.xlsx"
+					// 	b, err := ioutil.ReadFile(path)
+					// 	if err != nil {
+					// 		t.Errorf("expecting null err, got %q", err)
+					// 	}
+					// 	w.Write(b)
+					// 	return
+					// }
+					// if strings.Contains(uri, "proventos-de-todos-os-servidores-inativos") {
+					// 	path := "./output/proventos-de-todos-os-servidores-inativos-02-2019.xlsx"
+					// 	b, err := ioutil.ReadFile(path)
+					// 	if err != nil {
+					// 		t.Errorf("expecting null err, got %q", err)
+					// 	}
+					// 	w.Write(b)
+					// 	return
+					// }
+					// if strings.Contains(uri, "remuneracao-de-todos-os-membros-ativos") {
+					// 	path := "./output/proventos-de-todos-os-servidores-inativos-02-2019.xlsx"
+					// 	b, err := ioutil.ReadFile(path)
+					// 	if err != nil {
+					// 		t.Errorf("expecting null err, got %q", err)
+					// 	}
+					// 	w.Write(b)
+					// 	return
+					// }
+					path := "./output/proventos-de-todos-os-servidores-inativos-02-2019.xlsx"
 					b, err := ioutil.ReadFile(path)
 					if err != nil {
 						t.Errorf("expecting null err, got %q", err)
@@ -108,3 +107,11 @@ func TestIntegration(t *testing.T) {
 		})
 	}
 }
+
+/*
+/445-remuneracao-de-todos-os-membros-ativos?download=5051
+/448-proventos-de-todos-os-servidores-inativos?download=1098
+/447-proventos-de-todos-os-membros-inativos?download=4312
+/446-remuneracao-de-todos-os-servidores-atuvos?download=9999
+/449-valores-percebidos-por-todos-os-pensionistas?download=5453
+*/
