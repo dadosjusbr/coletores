cd3bf23e5056a02b28da8988613dfe8b141444a4
abuarquemf <abuarquemf@gmail.com>
2020-05-14 18:33:25 -0300

    fixed bugs

--- mppe/integration_test.go
+++ mppe/integration_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,21 +13,25 @@ import (
 	"github.com/dadosjusbr/storage"
 )
 
-//proventos-de-todos-os-servidores-inativos-02-2019.xlsx
+func printState(e storage.Employee) {
+	b, _ := json.Marshal(e)
+	fmt.Println(string(b))
+}
+
 func TestIntegration(t *testing.T) {
 	testCases := []struct {
-		name     string
-		month    int
-		year     int
-		server   *httptest.Server
-		employee storage.Employee
+		name       string
+		month      int
+		year       int
+		server     *httptest.Server
+		outputPath string
 	}{
 		{"Test for proventos-de-todos-os-servidores-inativos-02-2019", 2, 2019,
 			httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				// it was call to download the file
 				uri := r.URL.RequestURI()
 				if strings.Contains(uri, "download") {
-					path := "./test_files/proventos-de-todos-os-servidores-inativos-02-2019.xlsx"
+					path := "./test_files/sample.xlsx"
 					b, err := ioutil.ReadFile(path)
 					if err != nil {
 						t.Errorf("expecting null err, got %q", err)
@@ -44,54 +49,8 @@ func TestIntegration(t *testing.T) {
 					"<div><link src=\".../controller_servlet:download=5378:dea-022019\"/></div>"+
 					"<div><link src=\".../members_controller:code=8712:virt-fevereiro-2019\"/></div>")
 				return
-			})), storage.Employee{
-				Reg:       "1748491",
-				Name:      "ADOLFO VILANOVA DE ASSIS",
-				Active:    true,
-				Type:      "membro",
-				Workplace: "mppe",
-				Role:      "TECNICO MINIST SUPLEMENTAR                        ",
-				Discounts: &storage.Discount{
-					Total:            1748491,
-					PrevContribution: getPointer(1462.34),
-					CeilRetention:    getPointer(0),
-					IncomeTax:        getPointer(3208.91),
-					Others:           nil,
-				},
-				Income: &storage.IncomeDetails{
-					Total: 17171.58,
-					Wage:  getPointer(15155.98),
-					Other: &storage.Funds{
-						Total:            15155.98,
-						PersonalBenefits: nil,
-						EventualBenefits: nil,
-						PositionOfTrust:  nil,
-						Daily:            nil,
-						Gratification:    nil,
-						OriginPosition:   nil,
-						Others:           nil,
-					},
-					Perks: &storage.Perks{
-						Total:          500,
-						Food:           nil,
-						Transportation: nil,
-						PreSchool:      nil,
-						Health:         nil,
-						BirthAid:       nil,
-						HousingAid:     nil,
-						Subsistence:    nil,
-						Others: map[string]float64{
-							"christmasPerk":         0.0,
-							"indemnity":             500,
-							"loyaltyJob":            0.0,
-							"otherAmmounts":         1515.6,
-							"permanencePerk":        0.0,
-							"temporaryRemuneration": 0.0,
-							"vacacionPerk":          0.0,
-						},
-					},
-				},
-			}},
+			})), "./test_files/proventos-servidores-inativos.json",
+		},
 		// {"Test for remuneracao-de-todos-os-membros-ativos-02-2019", 2, 2019,
 		// 	httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 		// it was call to download the file
@@ -219,7 +178,7 @@ func TestIntegration(t *testing.T) {
 	}
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			c, e := Crawl("./output", 2, 2019, tt.server.URL+"/")
+			c, e := Crawl("./test_files", 2, 2019, tt.server.URL+"/")
 			if e != nil {
 				t.Errorf("want error nil, got %q", e)
 			}
@@ -227,25 +186,28 @@ func TestIntegration(t *testing.T) {
 			if e != nil {
 				t.Errorf("want error nil, got %q", e)
 			}
-			sample := r[0]
-			// Comparing sample with test object
-			if reflect.DeepEqual(sample, tt.employee) {
-				fmt.Println("equal")
+			testOutputSample := r[0]
+			desiredOutput, err := ioutil.ReadFile(tt.outputPath)
+			if err != nil {
+				t.Errorf("fail to read desired output file, got %q", err)
 			}
-
-			fmt.Println("not equal")
-			// if sample.Reg != tt.employee.Reg {
-			// 	t.Errorf("got %s, want %s", sample.Reg, tt.employee.Reg)
-			// }
-			// if sample.Name != tt.employee.Name {
-			// 	t.Errorf("got %s, want %s", sample.Name, tt.employee.Name)
-			// }
-			// if sample.Role != tt.employee.Role {
-			// 	t.Errorf("got %s, want %s", sample.Role, tt.employee.Role)
-			// }
-			// if sample.Workplace != tt.employee.Workplace {
-			// 	t.Errorf("got %s, want %s", sample.Workplace, tt.employee.Workplace)
-			// }
+			var res storage.Employee
+			err = json.Unmarshal(desiredOutput, &res)
+			if err != nil {
+				t.Errorf("want nil, got %q", err)
+			}
+			if res.Reg != testOutputSample.Reg {
+				t.Errorf("reg was supposed to be %s, got %s", testOutputSample.Reg, res.Reg)
+			}
+			if res.Name != testOutputSample.Name {
+				t.Errorf("name was supposed to be %s, got %s", testOutputSample.Name, res.Name)
+			}
+			if !reflect.DeepEqual(res.Discounts, testOutputSample.Discounts) {
+				t.Errorf("expect discounts to be equal, got differents")
+			}
+			if !reflect.DeepEqual(res.Income, testOutputSample.Income) {
+				t.Errorf("expect incomes to be equal, got differents")
+			}
 		})
 	}
 }
