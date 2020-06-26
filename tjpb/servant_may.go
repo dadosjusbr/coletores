b9f5335022af300cfb2ccb381c36eb11e820ed49
Joeberth Souza <joeberth.souza@ccc.ufcg.edu.br>
2020-06-26 03:50:45 -0300

    Generate first servants csv

--- tjpb/servant_may.go
+++ tjpb/servant_may.go
@@ -14,14 +14,14 @@ import (
 )
 
 func parserServerMay(path string) ([]storage.Employee, error) {
-	templateArea := []string{"88.928,16.838,557.246,102.083",
-		"88.928,97.873,546.722,213.637",
-		"89.98,207.323,557.246,360.973",
-		"93.137,358.868,557.246,815.61"}
+	templateArea := []string{"92.085,16.838,559.351,102.083",
+		"93.137,102.083,541.46,211.532",
+		"94.19,209.428,547.774,824.029",
+		"94.19,360.973,548.827,818.767"}
 	var csvByte [][]byte
 	var csvFinal [][]string
-	for _, templ := range templateArea {
-		cmdList := strings.Split(fmt.Sprintf(`java -jar tabula-1.0.3-jar-with-dependencies.jar -a %v -p all %v`, templ, filepath.Base(path)), " ")
+	for i, templ := range templateArea {
+		cmdList := strings.Split(fmt.Sprintf(`java -jar tabula-1.0.3-jar-with-dependencies.jar -t -a %v -p all %v`, templ, filepath.Base(path)), " ")
 		cmd := exec.Command(cmdList[0], cmdList[1:]...)
 		var outb, errb bytes.Buffer
 		cmd.Stdout = &outb
@@ -34,11 +34,15 @@ func parserServerMay(path string) ([]storage.Employee, error) {
 			//TODO CHECK ERROR
 			os.Exit(1)
 		}
-		if len(rows[0]) > 1 {
+		if i == 2 {
+			rows = treatDoubleLines(rows)
+		}
+		if i == 3 {
 			rows = fixNumberColumns(rows)
 		}
 		csvFinal = appendCSVColumns(csvFinal, rows)
 	}
+
 	file, err := os.Create(strings.Replace(filepath.Base(path), ".pdf", ".csv", 1))
 	if err != nil {
 		logError("Error creating csv: %q", err)
@@ -47,22 +51,28 @@ func parserServerMay(path string) ([]storage.Employee, error) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 	writer.WriteAll(csvFinal)
+
 	return []storage.Employee{}, nil
 }
 
 //TODO UNIT TEST
 //appendCSVColumns receives a base csv and append columns of a new csv to the right.
-func appendCSVColumns(baseCSV, newCols [][]string) [][]string {
-	if len(baseCSV) == 0 {
-		baseCSV = newCols
+func appendCSVColumns(mountedCsv, csv [][]string) [][]string {
+	if len(mountedCsv) == 0 {
+		for i := range csv {
+			if strings.Contains(csv[i][0], "Legenda das") {
+				break
+			}
+			mountedCsv = append(mountedCsv, csv[i])
+		}
 	} else {
-		for i := 0; i < len(newCols); i++ {
+		for i := 0; i < len(mountedCsv); i++ {
 			var newElement []string
-			newElement = append(newElement, newCols[i]...)
-			baseCSV[i] = append(baseCSV[i], newElement...)
+			newElement = append(newElement, csv[i]...)
+			mountedCsv[i] = append(mountedCsv[i], newElement...)
 		}
 	}
-	return baseCSV
+	return mountedCsv
 }
 
 //TODO Unit Test
@@ -75,4 +85,28 @@ func fixNumberColumns(rows [][]string) [][]string {
 		}
 	}
 	return rows
+}
+
+//TODO Unit Test
+//treatDoubleLines fix cels widh double lines based in other colunm without double line.
+func treatDoubleLines(rows [][]string) [][]string {
+	reg := regexp.MustCompile(`(\d+)( *[\.,]( *\d*( *\d))| +)+`)
+	var fixedCsv [][]string
+	for i := range rows {
+		rows[i][0] = reg.ReplaceAllString(rows[i][0], "")
+		fmt.Println(rows[i][0])
+		rowFixed := []string{}
+		if strings.Contains(rows[i][0], "ras desta natureza") {
+			return fixedCsv
+		}
+		fmt.Println(i)
+		if rows[i][2] == "" {
+			rows[i+1][0] = rows[i][0] + rows[i+1][0]
+			continue
+		}
+		fmt.Println(i)
+		rowFixed = append(rowFixed, rows[i][0])
+		fixedCsv = append(fixedCsv, rowFixed)
+	}
+	return fixedCsv
 }
