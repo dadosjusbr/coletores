c0c291af3b37ef17d29d22d94371444b2f707da9
Matheus Filipe <matheus.souza@ccc.ufcg.edu.br>
2020-10-25 16:09:19 -0300

    abc

--- mpsp/parser.py
+++ mpsp/parser.py
@@ -1,5 +1,18 @@
+#_*_ coding: utf-8 _*_
 import pandas
 from pandas_ods_reader import read_ods
+from dotenv import load_dotenv
+import csv
+import os
 
-def parsear(caminho): 
-    
+
+load_dotenv()
+arquivo = os.getenv("EXAMPLE")
+
+def parsear(arquivo): 
+    with open(arquivo, encoding='utf8', errors='ignore') as file:
+        read = csv.reader(file)
+        for line in read:
+            print(line)
+
+parsear(arquivo)
