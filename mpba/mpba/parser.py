5747f2b79c1fe8a9391efd4a5d70e33be79bef64
Ana Paula Gomes <apgomes88@gmail.com>
2020-12-17 15:31:40 +0100

    Add parser

---
+++ mpba/mpba/parser.py
@@ -0,0 +1,60 @@
+def parse(payload):
+    employees = []
+    for item in payload:
+        perks = {
+            "total": item["vlIdenizacoes"],
+            "food": None,
+            "vacation": item["vlRendFerias"],
+            "transportation": None,
+            "pre_school": None,
+            "health": None,
+            "birth_aid": None,
+            "housing_aid": None,
+            "subsistence": None,
+            "compensatory_leave": None,
+            "pecuniary": None,
+            "vacation_pecuniary": None,
+            "furniture_transport": None,
+            "premium_license_pecuniary": None,
+        }
+        funds = {
+            "total": item["vlRendTotalLiquido"],
+            "personal_benefits": item["vlRendAbonoPerman"],
+            "eventual_benefits": item["vlIdenizacoes"],
+            "trust_position": item["vlRendCargoComissao"],
+            "daily": None,
+            "gratification": item["vlRendGratNatalina"],
+            "origin_pos": item["vlRendTotalBruto"],
+            "others_total": item["vlRendVerbas"],
+            "others": item["vlOutrasRemun"],
+        }
+        income = {
+            "total": None,
+            "wage": item["vlRendCargoEfetivo"],
+            "perks": perks,
+            "other": funds,
+        }
+        discounts = {
+            "total": item["vlDescTotalBruto"],
+            "prev_contribution": item["vlDescPrevidencia"],
+            "ceil_retention": item["vlDescTeto"],
+            "income_tax": item["vlDescIR"],
+            "others_total": None,
+            "other": None,
+        }
+        employee = {
+            "reg": item["nuMatricula"],
+            "name": item["nmServidor"],
+            "role": item["dsCargo"],
+            "type": None,
+            "workplace": item["dsLotacao"],
+            "active": _status(item["dsLotacao"]),
+            "income": income,
+            "discounts": discounts,
+        }
+        employees.append(employee)
+    return employees
+
+
+def _status(workplace):
+    return workplace.lower() != "inativo"
---
+++ mpba/tests/test_parser.py
@@ -0,0 +1,103 @@
+from mpba.parser import parse, _status
+import pytest
+import json
+
+
+payload = [
+    {
+        "dsCargo": "DIGITADOR",
+        "dsLotacao": "PJR DE PAULO AFONSO - APOIO TECNICO E ADMINISTRATIVO",
+        "nmServidor": "IGOR ANDREYSON MENDES LOPES",
+        "nuAnoReferencia": 2020,
+        "nuMatricula": 904023,
+        "nuMesReferencia": 1,
+        "vlDescIR": 2428.29,
+        "vlDescPrevidencia": 1952.1,
+        "vlDescTeto": 0.0,
+        "vlDescTotalBruto": 4380.39,
+        "vlIdenizacoes": 1300.0,
+        "vlOutrasRemun": 0.0,
+        "vlRendAbonoPerman": 0.0,
+        "vlRendCargoComissao": 7093.93,
+        "vlRendCargoEfetivo": 6392.39,
+        "vlRendFerias": 0.0,
+        "vlRendGratNatalina": 0.0,
+        "vlRendTotalBruto": 15243.57,
+        "vlRendTotalLiquido": 10863.18,
+        "vlRendVerbas": 457.25,
+    }
+]
+
+
+def test_parse_payload_to_employee():
+    expected_perks = {
+        "total": 1300.0,
+        "food": None,
+        "vacation": 0.0,
+        "transportation": None,
+        "pre_school": None,
+        "health": None,
+        "birth_aid": None,
+        "housing_aid": None,
+        "subsistence": None,
+        "compensatory_leave": None,
+        "pecuniary": None,
+        "vacation_pecuniary": None,
+        "furniture_transport": None,
+        "premium_license_pecuniary": None,
+    }
+    expected_funds = {
+        "total": 10863.18,
+        "personal_benefits": 0.0,
+        "eventual_benefits": 1300.0,
+        "trust_position": 7093.93,
+        "daily": None,
+        "gratification": 0.0,
+        "origin_pos": 15243.57,
+        "others_total": 457.25,
+        "others": 0.0,
+    }
+    expected_discounts = {
+        "total": 4380.39,
+        "prev_contribution": 1952.1,
+        "ceil_retention": 0.0,
+        "income_tax": 2428.29,
+        "others_total": None,
+        "other": None,
+    }
+    expected_income = {
+        "total": None,
+        "wage": 6392.39,
+        "perks": expected_perks,
+        "other": expected_funds,
+    }
+    expected_employee = {
+        "reg": 904023,
+        "name": "IGOR ANDREYSON MENDES LOPES",
+        "role": "DIGITADOR",
+        "type": None,
+        "workplace": "PJR DE PAULO AFONSO - APOIO TECNICO E ADMINISTRATIVO",
+        "active": True,
+        "income": expected_income,
+        "discounts": expected_discounts,
+    }
+
+    assert parse(payload) == [expected_employee]
+
+
+def test_raise_exception_when_payload_is_invalid():
+    with pytest.raises(Exception):
+        parse(None)
+
+
+@pytest.mark.parametrize(
+    "workplace,expected_status",
+    [
+        ("PJR DE PAULO AFONSO - APOIO TECNICO E ADMINISTRATIVO", True),
+        ("Inativo", False),
+        ("NACRES - PERICIA", True),
+        ("INATIVO", False),
+    ],
+)
+def test_get_status(workplace, expected_status):
+    assert _status(workplace) is expected_status
