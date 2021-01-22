fc960ceb84a06667820599ef8efa6687ba7af1cc
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2021-01-22 12:40:09 -0300

    Parse para membros e servidores

--- mpsp/src/parser.py
+++ mpsp/src/parser.py
@@ -7,7 +7,6 @@ import math
 import sys
 import os
 import pathlib
-import numpy as np
 
 cwd = os.getcwd()
 
@@ -54,76 +53,99 @@ def get_end_row(rows, begin_row, file_name):
    
     return end_row 
 
+def type_employee(fn):
+    if 'Membros' in fn:
+        return 'membro'
+    if 'Servidores' in fn:
+        return 'servidor'
+    if 'Pensionistas' in fn:
+        return 'pensionista'
+    if 'Colaboradores' in fn:
+        return 'colaborador'
+    raise ValueError('Tipo de inválido de funcionário público: ' + fn)
+
+
 def parse_employees(file_name):
     rows = read_data(file_name).to_numpy()
 
     begin_row = get_begin_row(rows)
     end_row = get_end_row(rows, begin_row, file_name)
    
-    # typeE = type_employee(file_name)
-    # activeE = 'inativos' not in file_name and 'Pensionistas' not in file_name
-    # employees = {}
-    # curr_row = 0
-    # for row in rows:
-    #     if curr_row < begin_row:
-    #         curr_row += 1
-    #         continue
+    typeE = type_employee(file_name)
+    activeE = 'inativos' not in file_name and 'Pensionistas' not in file_name
+    employees = {}
+    curr_row = 0
+    for row in rows:
+        if curr_row < begin_row:
+            curr_row += 1
+            continue
 
-    #     employees[row[0]] = {
-    #         'reg': row[0],
-    #         'name': row[1],
-    #         'role': row[2],
-    #         'type': typeE,
-    #         'workplace': row[3],
-    #         'active': activeE,
-    #         "income":
-    #         {
-    #             'total': row[12],
-    #             # REMUNERAÇÃO BÁSICA = Remuneração Cargo Efetivo + Outras Verbas Remuneratórias, Legais ou Judiciais
-    #             'wage': row[4]+row[5],
-    #             'perks': {
-    #                 'total': row[11],
-    #             },
-    #             'other':
-    #             {  # Gratificações
-    #                 'total': row[6] + row[7] + row[8]+row[9],
-    #                 'trust_position': row[6],
-    #                 'others_total': row[7]+row[8]+row[9],
-    #                 'others': {
-    #                     'Gratificação Natalina': row[7],
-    #                     'Férias (1/3 constitucional)': row[8],
-    #                     'Abono de Permanência': row[9],
-    #                 }
-    #             },
-    #         },
-    #         'discounts':
-    #         {  # Discounts Object. Using abs to garantee numbers are positivo (spreadsheet have negative discounts).
-    #             'total': abs(row[16]),
-    #             'prev_contribution': abs(row[13]),
-    #             # Retenção por teto constitucional
-    #             'ceil_retention': abs(row[15]),
-    #             'income_tax': abs(row[14]),
-    #         }
-    #     }
+        employees[row[0]] = {
+            'reg': row[0],
+            'name': row[1],
+            'role': row[2],
+            'type': typeE,
+            'workplace': row[3],
+        
+            #'active': activeE,
+            "income":
+            {
+                'total': row[12],
+                # REMUNERAÇÃO BÁSICA = Remuneração Cargo Efetivo + Outras Verbas Remuneratórias, Legais ou Judiciais
+                'wage': row[4]+row[5],
+                'perks': {
+                    'total': row[11],
+                },
+                'other':
+                {  # Gratificações
+                    'total': row[6] + row[7] + row[8]+row[9],
+                    'trust_position': row[6],
+                    'others_total': row[7]+row[8]+row[9],
+                    'others': {
+                        'Gratificação Natalina': row[7],
+                        'Férias (1/3 constitucional)': row[8],
+                        'Abono de Permanência': row[9],
+                    }
+                },
+            },
+            'discounts':
+            {  # Discounts Object. Using abs to garantee numbers are positivo (spreadsheet have negative discounts).
+                'total': abs(row[16]),
+                'prev_contribution': abs(row[13]),
+                # Retenção por teto constitucional
+                'ceil_retention': abs(row[15]),
+                'income_tax': abs(row[14]),
+            }
+        }
 
-    #     curr_row += 1
-    #     if curr_row >= end_row:
-    #         break
+        curr_row += 1
+        if curr_row >= end_row:
+            break
 
-    return {'Sex': 'female', 'Age': 7, 'Name': 'Zara'}
+    return employees
+
+
 
 def parse(file_names):
     print('FILES_NAMES')
     print(file_names)
     employees = {}
     for fn in file_names:
-        if 'Verbas Indenizatorias' not in fn:
+        if 'Verbas Indenizatorias' not in fn and 'Pensionistas' not in fn:
             # Puts all parsed employees in the big map
-           # print("PARSEE EMPLOYEE")
-            #print(parse_employees(fn))
+   
             employees.update(parse_employees(fn))
-   # print("EMPLOYEE")
-   # print(employees)
+        # if 'Pensionistas' in fn and 'membros' in fn:
+        #     # Puts all parsed employees in the big map
+   
+        #     employees.update(parse_employees_pensionistas_membros(fn))
+
+        # if 'Pensionistas' in fn and 'servidores' in fn:
+        #     # Puts all parsed employees in the big map
+   
+        #     employees.update(parse_employees_pensionistas_servidores(fn))
+
+
     # try:
     #     for fn in file_names:
     #         if 'Verbas Indenizatorias' in fn:
@@ -132,5 +154,6 @@ def parse(file_names):
     #     sys.stderr.write('Registro inválido ao processar verbas indenizatórias: {}'.format(e))
     #     sys.stderr.write('Mapa de funcionários: {}'.format(employees))
     #     os._exit(1)
-
+    
+    print(employees)
     return list(employees.values())
