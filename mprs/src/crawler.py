a56064d00ac121eb35a0f4e1199325c8e183579e
diegooalmeida <diego.almeida@ccc.ufcg.edu.br>
2021-01-26 15:09:16 -0300

    Adiciona crawler

---
+++ mprs/src/crawler.py
@@ -0,0 +1,56 @@
+import requests
+import pathlib
+
+beneficiary_types = {1: 'M/',
+                     2: 'MI/',
+                     3: 'S/',
+                     4: 'SI/',
+                     5: 'P/',
+                     6: 'estagiarios/',
+                     7: 'verbas_indenizatorias_temporarias/'
+                    }
+
+payroll_types = {1: 'NORMAL/',
+                 2: 'COMPLEMENTAR/',
+                 3: '13/'
+                }
+
+base_URL = 'https://transparencia.mprs.mp.br/contracheque/'
+
+# Gera a url de acesso às informações referentes as remunerações do mês e ano de um tipo de funcionário
+def generate_remuneration_url(year, month):
+    links = {}
+    link = ""
+
+    for key in beneficiary_types:
+        if key < 6:
+            for value in payroll_types.values():
+                link = base_URL + "download/" + beneficiary_types[key] + year + "/" + month + "/" + value
+                links[beneficiary_types[key] + value] = link
+        elif key == 6:
+            link = base_URL + beneficiary_types[key] + "?ano=" + year + "&mes=" + month.lstrip("0") + "&procurar=+Procurar+#"
+            links[beneficiary_types[key]] = link
+        else:
+            link = base_URL + beneficiary_types[key] + year + "/" + month + "/#"
+            links[beneficiary_types[key]] = link
+
+    return links
+
+def download(url, file_path):
+    response = requests.get(url, allow_redirects=True, verify = False)
+    with open("./" + file_path, "wb") as file:
+        file.write(response.content)
+    file.close()
+
+def crawl(year, month, output_path):
+    urls_remunerations  = generate_remuneration_url(year,month)
+    files = []
+
+    for element in urls_remunerations:
+        pathlib.Path('./' + output_path).mkdir(exist_ok=True)
+        file_name = element + month + '-' + year + '.csv'
+        file_path = output_path + "/" + file_name.replace("/", "-")
+        download(urls_remunerations[element], file_path)
+        files.append(file_path)
+
+    return files
