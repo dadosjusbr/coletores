1c74cface8b0523fdb53f5ba80cfa0fd5b0e04df
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2021-01-28 14:37:36 -0300

    Parser e testes sem verbas indenizatórias

--- mprj/src/parser.py
+++ mprj/src/parser.py
@@ -5,6 +5,7 @@ import pathlib
 import sys
 import os
 
+
 #Lê os dados baixados pelo crawler
 def read_data(path):
     try:
@@ -26,13 +27,12 @@ def get_begin_row(rows, begin_string):
         begin_row += 1
         if row[0] == begin_string:
             break
-
     #Continua interando até encontrarmos um valor que não seja string em 
     #branco. Isto ocorre pelo formato da planilha
     while isNaN(rows[begin_row][0]):
         begin_row += 1
 
-    return begin_row 
+    return begin_row
 
 def get_end_row(rows, begin_row):
     end_row = 0
@@ -54,7 +54,7 @@ def type_employee(fn):
     if 'SATIV' in fn or 'SINAT' in fn:
         return 'servidor'
     if 'PENSI' in fn:
-        return 'pensionistas'
+        return 'pensionista'
     if 'COLAB' in fn:
         return 'colaborador'
     raise ValueError('Tipo inválido de funcionário público: ' + fn)
@@ -66,6 +66,10 @@ def clean_currency(value):
 
 def clean_df(data):
     for col in data.columns[4:]:
+        data[col] = data[col].apply(clean_currency)
+
+def clean_colab_df(data):
+    for col in data.columns[2:5]:
         data[col] = data[col].apply(clean_currency)
 
 def parse_employees(file_name):
@@ -117,12 +121,105 @@ def parse_employees(file_name):
             'discounts':
             {
                 'total': abs(float(row[16])),
-                'prev_contribution': abs(float(row[14])),
+                'prev_contribution': abs(float(row[13])),
                 'ceil_retention': abs(float(row[15])),
                 'income_tax': abs(float(row[14]))
             }
         }
 
+        curr_row += 1
+        if curr_row >= end_row:
+            break
+    
+    return employees
+
+def parse_colab(file_name):
+    rows  = read_data(file_name)
+    clean_colab_df(rows)
+    rows =  rows.to_numpy()
+
+    begin_string = 'LOTAÇÃO'
+    begin_row  =  get_begin_row(rows, begin_string)
+    end_row = get_end_row(rows, begin_row)
+
+    typeE = type_employee(file_name)
+    activeE = True
+    employees  = {}
+    curr_row = 0
+
+    for row in rows:
+        if curr_row < begin_row:
+            curr_row += 1
+            continue
+        
+        #O identificador de colaboradores é o nome 
+        employees[row[1]] = {
+            'name': row[1],
+            #Descrição  + Processo  + Competência
+            'role': str(row[9]) + ' ' + str(row[8]), 
+            'type': typeE,
+            'workplace': row[0],
+            'active': activeE,
+            "income":
+            {
+                'total': float(row[2]),
+            },
+            'discounts':
+            {
+                'total': abs(float(row[4])),
+                'income_tax': abs(float(row[3]))
+            }
+        }
+    
+        curr_row += 1
+        if curr_row >= end_row:
+            break
+    
+    return employees
+
+def update_employee_indemnity(file_name, employees):
+    rows  = read_data(file_name)
+    print(rows)
+    clean_df(rows)
+
+    begin_string = "MATRÍCULA"  # word before starting data
+    begin_row = get_begin_row(rows, begin_string)
+    end_row = get_end_row(rows, begin_row)
+    curr_row = 0
+
+    for row in rows:
+        if curr_row < begin_row:
+            curr_row += 1
+            continue
+
+        emp = employees[row[0]]
+        emp['income']['perks'].update({
+            #Auxilios saúde e alimentação estão dispostos em 2 colunas diferentes 
+            'food': float(row[4]) + float(row[11]),
+            'transportation': float(row[17]),
+            'health': float(row[6]) + float(row[13]),
+        })
+        emp['income']['other'].update({
+            'total': round (emp['income']['other']['total'] + float(row[5]) + float(row[12]) 
+            + float(row[7]) + float(row[8]) + float(row[9]) + float(row[10]) + float(row[14]) + float(row[15]) +
+            float(row[18]), 3),
+            'Gratification': float(row[16]),
+            'others_total': round(emp['income']['other']['others_total'] + float(row[5]) + float(row[12]) 
+            + float(row[7]) + float(row[8]) + float(row[9]) + float(row[10]) + float(row[14]) + float(row[15]) +
+            float(row[18]), 3),
+        })
+        emp['income']['other']['others'].update({
+            #Auxílio educação está disposto em 2 colunas diferentes 
+            'AUXÍLIO-EDUCAÇÃO': float(row[5]) + float(row[12]),
+            'CONVERSÃO DE LICENÇA ESPECIAL': float(row[7]),
+            'DEVOLUÇÃO IR RRA': float(row[8]),
+            'INDENIZAÇÃO DE FÉRIAS NÃO USUFRUÍDAS': float(row[9]),
+            'INDENIZAÇÃO POR LICENÇA NÃO GOZADA': float(row[10]),
+            'DEVOLUÇÃO FUNDO DE RESERVA': float(row[14]),
+            'DIFERENÇAS DE AUXÍLIOS': float(row[15]),
+            'PARCELAS PAGAS EM ATRASO': float(row[18])
+        })
+        employees[row[0]] = emp
         curr_row += 1
         if curr_row >= end_row:
             break
@@ -135,5 +232,11 @@ def parse(file_names):
     for fn in file_names:
         if ('Verbas Indenizatórias' not in fn) and ('COLAB' not in fn):
             employees.update(parse_employees(fn))
-    
-    return employees
+        elif ('COLAB' in fn):
+            employees.update(parse_colab(fn))
+
+    # for fn in file_names:
+    #     if 'Verbas Indenizatórias' in fn:
+    #         update_employee_indemnity(fn, employees)
+
+    return list(employees.values())
---
+++ mprj/src/parser_test.py
@@ -0,0 +1,258 @@
+import os
+import parser
+import unittest
+import pathlib
+import numpy as np
+
+class TestParser(unittest.TestCase):
+
+    def test_membros_ativos(self):
+        self.maxDiff = None 
+
+        expected = {
+            'reg': '02003042',
+            'name': 'ADELIA BARBOZA DE CARVALHO',
+            'role': 'PROCURADOR DE JUSTICA',
+            'type': 'membro',
+            'workplace':'1ª PROCURADORIA DE JUSTIÇA JUNTO À 22ª CÂMARA CÍVEL DO TRIBUNAL DE JUSTIÇA DO ESTADO DO RIO DE JANEIRO',
+            'active': True,
+            'income':
+                {
+                    'total': 84630.50,
+                    'wage': 37300.81,
+                    'perks':{
+                        'total':0.00,
+                    },
+                    'other':
+                    {
+                        'total': 5222.11,
+                        'trust_position': 0.00,
+                        'others_total': 5222.11,
+                        'others':{
+                            'Gratificação Natalina': 0.00,
+                            'Férias (1/3 constitucional)': 0.00,
+                            'Abono de Permanência': 5222.11,
+                        },
+                    },
+                },
+                'discounts':
+                {
+                    'total': 13174.39,
+                    'prev_contribution': 5222.11,
+                    'ceil_retention':0.00,
+                    'income_tax': 7952.28,
+                }
+        }
+        files = [('./output_test/2020_11_MATIV')]
+        employees = parser.parse(files)
+
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+    def test_membros_inativos(self):
+        self.maxDiff = None 
+
+        expected = {
+            'reg': '00179515',
+            'name': 'ADA BUKSMAN',
+            'role': 'PROMOTOR DE JUSTICA',
+            'type': 'membro',
+            'workplace': np.nan,
+            'active': False,
+            'income':
+                {
+                    'total': 45760.30,
+                    'wage': 35462.22,
+                    'perks':{
+                        'total':0.00,
+                    },
+                    'other':
+                    {
+                        'total': 0.00,
+                        'trust_position': 0.00,
+                        'others_total': 0.00,
+                        'others':{
+                            'Gratificação Natalina': 0.00,
+                            'Férias (1/3 constitucional)': 0.00,
+                            'Abono de Permanência': 0.00,
+                        },
+                    },  
+                },
+                'discounts':
+                {
+                    'total': 4438.49,
+                    'prev_contribution': 4438.49,
+                    'ceil_retention':0.00,
+                    'income_tax': 0.00,
+                }
+        }
+        files = [('./output_test/2020_11_MINAT')]
+        employees = parser.parse(files)
+
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+    def test_servidores_ativos(self):
+        self.maxDiff = None
+
+        expected = {
+            'reg': '00003189',
+            'name': 'ACELINO AMURIM DA SILVA',
+            'role': 'TÉCNICO DO MP - ÁREA: PROCESSUAL',
+            'type': 'servidor',
+            'workplace': 'SECRETARIA DA 2ª PROMOTORIA DE JUSTIÇA CÍVEL DE DUQUE DE CAXIAS',
+            'active': True,
+            'income':
+                {
+                    'total': 17119.27,
+                    'wage': 11644.51,
+                    'perks':{
+                        'total':1477.75,
+                    },
+                    'other':
+                    {
+                        'total': 0.00,
+                        'trust_position': 0.00,
+                        'others_total': 0.00,
+                        'others':{
+                            'Gratificação Natalina': 0.00,
+                            'Férias (1/3 constitucional)': 0.00,
+                            'Abono de Permanência': 0.00,
+                        },
+                    },  
+                },
+                'discounts':
+                {
+                    'total': 3763.91,
+                    'prev_contribution': 1630.23,
+                    'ceil_retention':0.00,
+                    'income_tax': 2133.68,
+                }
+        }
+        files = [('./output_test/2020_11_SATIV')]
+        employees = parser.parse(files)
+
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+    
+    def teste_servidores_inativos(self):
+        self.maxDiff = None
+
+        expected = {
+            'reg': '00001322',
+            'name': 'ADELAIDE BURATTO',
+            'role': 'AUXILIAR DO MP - ÁREA: ADMINISTRATIVA',
+            'type': 'servidor',
+            'workplace': np.nan,
+            'active': False,
+            'income':
+                {
+                    'total': 9548.15,
+                    'wage': 8827.26,
+                    'perks':{
+                        'total':0.00,
+                    },
+                    'other':
+                    {
+                        'total': 0.00,
+                        'trust_position': 0.00,
+                        'others_total': 0.00,
+                        'others':{
+                            'Gratificação Natalina': 0.00,
+                            'Férias (1/3 constitucional)': 0.00,
+                            'Abono de Permanência': 0.00,
+                        },
+                    },  
+                },
+                'discounts':
+                {
+                    'total': 1311.25,
+                    'prev_contribution': 381.67,
+                    'ceil_retention': 0.00,
+                    'income_tax': 929.58,
+                }
+        }
+        files = [('./output_test/2020_11_SINAT')]
+        employees = parser.parse(files)
+
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+    def teste_pensionistas(self):
+        self.maxDiff = None
+
+        expected = {
+            'reg': '02002761-1',
+            'name':'ACASSIA MARIA CARVALHO PEREIRA',
+            'role': np.nan,
+            'type': 'pensionista',
+            'workplace': np.nan,
+            'active': False,
+            'income':
+                {
+                    'total': 26473.52,
+                    'wage': 26473.52,
+                    'perks':{
+                        'total':0.00,
+                    },
+                    'other':
+                    {
+                        'total': 0.00,
+                        'trust_position': 0.00,
+                        'others_total': 0.00,
+                        'others':{
+                            'Gratificação Natalina': 0.00,
+                            'Férias (1/3 constitucional)': 0.00,
+                            'Abono de Permanência': 0.00,
+                        },
+                    },  
+                },
+                'discounts':
+                {
+                    'total': 7955.07,
+                    'prev_contribution': 2852.15,
+                    'ceil_retention': 0.00,
+                    'income_tax': 5102.92,
+                }
+        }
+
+        files = [('./output_test/2020_11_PENSI')]
+        employees = parser.parse(files)
+
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+    def teste_colaborador(self):
+        self.maxDiff = None
+        
+        expected = {
+            'name': 'AGHATTA CRYST DE BRITO GONÇALVES',
+            'role': 'Bolsa auxílio ao estagiário forense. 20.22.0001.0028176.2020-34',
+            'type': 'colaborador',
+            'workplace': 'PROMOTORIA DE JUSTIÇA CÍVEL E DE FAMÍLIA DE RIO DAS OSTRAS',
+            'active': True,
+            'income':
+            {
+                'total': 870.00,
+            },
+            'discounts':
+            {
+                'total': 0.00,
+                'income_tax': 0.00,
+            }
+        }
+
+        files = [('./output_test/2020_11_COLAB')]
+        employees = parser.parse(files)
+    
+        #Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+if __name__ == '__main__':
+    unittest.main()
