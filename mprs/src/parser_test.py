16c98147258ee25466cb85b4bb408cbb6050798a
diegooalmeida <diego.almeida@ccc.ufcg.edu.br>
2021-02-08 18:23:52 -0300

    Corrige antigo employee e adiciona mais um ao teste

--- mprs/src/parser_test.py
+++ mprs/src/parser_test.py
@@ -14,10 +14,10 @@ class TestParser(unittest.TestCase):
             'workplace': 'PROMOTORIA DE JUSTIÇA DE PINHEIRO MACHADO',
             'active': True,
             'income': {
-                'total': 25851.96,
+                'total': 24172.19,
                 'wage': 25851.96,
                 'perks': {
-                    'total': 0.0
+                    'total': 620.51
                 },
                 'other': {
                     'total': 0.0,
@@ -31,10 +31,42 @@ class TestParser(unittest.TestCase):
                 }
             },
             'discounts': {
-                'total': 9094.28,
+                'total': 8253.96,
                 'prev_contribution': 4008.95,
                 'ceil_retention': 0.0,
-                'income_tax': 5085.33
+                'income_tax': 4245.01
+            }
+        }
+
+        second_expected = {
+            'reg': '3427811',
+            'name': 'BRUNO HERINGER JUNIOR', 
+            'role': 'PROMOTOR DE JUSTICA DE ENTRÂNCIA FINAL', 
+            'type': 'membro', 
+            'workplace': 'SUBPROCURADORIA-GERAL DE JUSTIÇA PARA ASSUNTOS JURÍDICOS', 
+            'active': True, 
+            'income': {
+                'total': 39563.9, 
+                'wage': 31916.0, 
+                'perks': {
+                    'total': 910.08
+                }, 
+                'other': {
+                    'total': 6737.82, 
+                    'trust_position': 3191.6, 
+                    'others_total': 3546.22, 
+                    'others': {
+                        'Gratificação Natalina': 0.0, 
+                        'Férias (1/3 constitucional)': 3546.22, 
+                        'Abono de Permanência': 0.0
+                    }
+                }
+            }, 
+            'discounts': {
+                'total': 12632.88, 
+                'prev_contribution': 5161.11, 
+                'ceil_retention': 0.0, 
+                'income_tax': 7471.77
             }
         }
 
@@ -42,8 +74,9 @@ class TestParser(unittest.TestCase):
         employees = parser.parse(files)
 
         # Verificações
-        self.assertEqual(1, len(employees))
+        self.assertEqual(2, len(employees))
         self.assertDictEqual(employees[0], expected)
+        self.assertDictEqual(employees[1], second_expected)
 
     # Membros ativos com folha complementar
     def test_membros_ativos_complementar(self):
@@ -57,10 +90,10 @@ class TestParser(unittest.TestCase):
             'workplace': 'PROMOTORIA DE JUSTIÇA DE PINHEIRO MACHADO',
             'active': True,
             'income': {
-                'total': 51703.92,
+                'total': 50024.149999999994,
                 'wage': 25851.96,
                 'perks': {
-                    'total': 25851.96
+                    'total': 26472.469999999998
                 },
                 'other': {
                     'total': 0.0,
@@ -74,10 +107,42 @@ class TestParser(unittest.TestCase):
                 }
             },
             'discounts': {
-                'total': 9094.28,
+                'total': 8253.96,
                 'prev_contribution': 4008.95,
                 'ceil_retention': 0.0,
-                'income_tax': 5085.33
+                'income_tax': 4245.01
+            }
+        }
+
+        second_expected = {
+            'reg': '3427811',
+            'name': 'BRUNO HERINGER JUNIOR', 
+            'role': 'PROMOTOR DE JUSTICA DE ENTRÂNCIA FINAL', 
+            'type': 'membro', 
+            'workplace': 'SUBPROCURADORIA-GERAL DE JUSTIÇA PARA ASSUNTOS JURÍDICOS', 
+            'active': True, 
+            'income': {
+                'total': 75167.97, 
+                'wage': 31916.0, 
+                'perks': {
+                    'total': 36514.15
+                }, 
+                'other': {
+                    'total': 6737.82, 
+                    'trust_position': 3191.6, 
+                    'others_total': 3546.22, 
+                    'others': {
+                        'Gratificação Natalina': 0.0, 
+                        'Férias (1/3 constitucional)': 3546.22, 
+                        'Abono de Permanência': 0.0
+                    }
+                }
+            }, 
+            'discounts': {
+                'total': 12632.88, 
+                'prev_contribution': 5161.11, 
+                'ceil_retention': 0.0, 
+                'income_tax': 7471.77
             }
         }
 
@@ -86,8 +151,9 @@ class TestParser(unittest.TestCase):
         employees = parser.parse(files)
 
         # Verificações
-        self.assertEqual(1, len(employees))
+        self.assertEqual(2, len(employees))
         self.assertDictEqual(employees[0], expected)
+        self.assertDictEqual(employees[1], second_expected)
 
     # Membros ativos com folha complementar e 13º
     def test_membros_ativos_decimo(self):
@@ -101,10 +167,10 @@ class TestParser(unittest.TestCase):
             'workplace': 'PROMOTORIA DE JUSTIÇA DE PINHEIRO MACHADO',
             'active': True,
             'income': {
-                'total': 77555.88,
+                'total': 75876.10999999999,
                 'wage': 51703.92,
                 'perks': {
-                    'total': 25851.96
+                    'total': 26472.469999999998
                 },
                 'other': {
                     'total': 0.0,
@@ -118,10 +184,42 @@ class TestParser(unittest.TestCase):
                 }
             },
             'discounts': {
-                'total': 18188.56,
+                'total': 17348.239999999998,
                 'prev_contribution': 8017.9,
                 'ceil_retention': 0.0,
-                'income_tax': 10170.66
+                'income_tax': 9330.34
+            }
+        }
+
+        second_expected = {
+            'reg': '3427811',
+            'name': 'BRUNO HERINGER JUNIOR', 
+            'role': 'PROMOTOR DE JUSTICA DE ENTRÂNCIA FINAL', 
+            'type': 'membro', 
+            'workplace': 'SUBPROCURADORIA-GERAL DE JUSTIÇA PARA ASSUNTOS JURÍDICOS', 
+            'active': True, 
+            'income': {
+                'total': 110275.57, 
+                'wage': 63832.0, 
+                'perks': {
+                    'total': 36514.15
+                }, 
+                'other': {
+                    'total': 9929.42, 
+                    'trust_position': 6383.2, 
+                    'others_total': 3546.22, 
+                    'others': {
+                        'Gratificação Natalina': 0.0, 
+                        'Férias (1/3 constitucional)': 3546.22, 
+                        'Abono de Permanência': 0.0
+                    }
+                }
+            }, 
+            'discounts': {
+                'total': 25159.92, 
+                'prev_contribution': 10322.23, 
+                'ceil_retention': 0.0, 
+                'income_tax': 14837.69
             }
         }
 
@@ -131,8 +229,9 @@ class TestParser(unittest.TestCase):
         employees = parser.parse(files)
 
         # Verificações
-        self.assertEqual(1, len(employees))
+        self.assertEqual(2, len(employees))
         self.assertDictEqual(employees[0], expected)
+        self.assertDictEqual(employees[1], second_expected)
 
 if __name__ == '__main__':
     unittest.main()
