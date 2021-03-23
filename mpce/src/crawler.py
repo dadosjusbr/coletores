32d06f54dcb12dc1cb79f8285ccbe9123ca5f046
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2021-03-23 15:19:23 -0300

    Coletando Verbas indenizatórias de membros

--- mpce/src/crawler.py
+++ mpce/src/crawler.py
@@ -7,10 +7,22 @@ from urllib.request import urlopen
 
 base_url = 'http://wapp.mpce.mp.br/PortalTransparenciaConsultas/Visao/extratos.aspx{}'
 
+url_formats = {
+    'remu':'?opt=1',
+    'vi':'?opt=9'
+    }
+
+url_params = {
+    'remu': (('opt','1'),),
+    'vi': (('opt', '9'),)
+}
+
 #Armazena informações para o envio de requisições referente aos formatos 
 class request:
-    def __init__(self, url):
+
+    def __init__(self, url, params):
         self.url = url
+        self.params = params
 
     def make_payload(self, viewstate, viewstate_gen, event_validation, ddlmes, ddlano, txtNome):
         self.payload = {
@@ -28,7 +40,7 @@ class request:
 
 def download(request, file_path):
     try:
-      response = requests.post(request.url , params=(('opt','1'),), data=request.payload, allow_redirects=True)
+      response = requests.post(request.url , params=request.params, data=request.payload, allow_redirects=True)
     except Exception as excep:
         sys.stderr.write("Não foi possível fazer o download do arquivo: " + file_path + ' . A requisição foi enviada para a url: ' + request_data['url'] + ' . E o foi retornado status code:' + response.status_code)
     try:    
@@ -40,33 +52,39 @@ def download(request, file_path):
         os._exit(1)
 
 #Este metódo seta os valores necessários para o envio da requisição http
-def init_request(year, month):
+def init_requests(year, month):
+    requests = {}
 
-    #Adquire os valores referentes ás variáveis __VIEWSTATEGENERATOR _VIEWSTATE e _EVENTVALIDATION
-    url_key = base_url.format('?opt=1')
-    page = urlopen(url_key)
-    soup = BeautifulSoup(page, features='lxml')
+    for key in url_formats:
+        #Adquire os valores referentes ás variáveis __VIEWSTATEGENERATOR _VIEWSTATE e _EVENTVALIDATION
+        url_key = base_url.format(url_formats[key])
+        page = urlopen(url_key)
+        soup = BeautifulSoup(page, features='lxml')
 
-    view_generator = soup.body.find('input',{'id': '__VIEWSTATEGENERATOR'}).get('value')
-    view_state = soup.body.find('input',{'id': '__VIEWSTATE'}).get('value')
-    event_validation = soup.body.find('input',{'id': '__EVENTVALIDATION'}).get('value')
+        view_generator = soup.body.find('input',{'id': '__VIEWSTATEGENERATOR'}).get('value')
+        view_state = soup.body.find('input',{'id': '__VIEWSTATE'}).get('value')
+        event_validation = soup.body.find('input',{'id': '__EVENTVALIDATION'}).get('value')
 
-    #Cria objeto referente á requisição
-    request_obj = request(base_url.format(''))
-    request_obj.make_payload(view_state, view_generator, event_validation, month, year, '')
+        #Cria objeto referente á requisição
+        params  = url_params[key]
+        request_obj = request(base_url.format(''), params)
+        request_obj.make_payload(view_state, view_generator, event_validation, month, year, '')
 
-    return request_obj
+        requests[key] = request_obj
+
+    return requests
 
 def crawl(year, month, output_path):
     files = []
 
     # Realizando download da folha de remunerações simples 
-    request = init_request(year, month)        
-    pathlib.Path(output_path).mkdir(exist_ok=True)
-    filename = year + '_' + month + '_' + 'remu'
-    file_path =  output_path + '/' + filename + '.html'
-    download(request, file_path)
-    
+    requests = init_requests(year, month)
+    for key in requests:        
+        pathlib.Path(output_path).mkdir(exist_ok=True)
+        filename = year + '_' + month + '_' + key
+        file_path =  output_path + '/' + filename + '.html'
+        download(requests[key], file_path)
+        
     files.append(file_path)
 
     return files
