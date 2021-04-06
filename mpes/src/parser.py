7c4a06e2a393aa3865306b8f50554e3c53b885d9
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2021-04-06 17:43:48 -0300

    Parser funcionando para meses após agosto de 2019

--- mpes/src/parser.py
+++ mpes/src/parser.py
@@ -1,2 +1,149 @@
-def parse(file_names):
-    return []
+import sys
+import pandas as pd
+import os
+
+def read(path):
+    try:
+        data = pd.read_excel(path, engine='openpyxl')
+        return data
+    except Exception as excep:
+        sys.stderr.write("'Não foi possível ler o arquivo: " + path + '. O seguinte erro foi gerado: ' + excep)
+        os._exit(1)
+
+def clean_currency(data, beg_col, end_col):
+    for col in data.columns[beg_col:end_col]:
+        data[col] = data[col].apply(clean_currency_val)
+
+def clean_currency_val(value):
+    return float(value)
+
+
+def employees_parser(file_path):
+    data = read(file_path)
+
+    #Ajustando dataframe para simplificar interação
+    data = data[data['Unnamed: 0'].notna()]
+    data = data[:-1]
+    data = data[1:]
+    clean_currency(data,4,14,)
+
+    #Parsing data
+    rows = data.to_numpy()
+    employees = {}
+
+    for row in rows:
+        reg = str(row[0]) #Matrícula 
+        name = row[1] #Nome
+        role = row[2] #Cargo
+        workplace = row[3] #Lotação
+        remuneration = row[4] #Remuneração do cargo efetivo
+        other_verbs =  row[5] #Outras Verbas Remuneratórias, Legais ou Judiciais   
+        trust_pos = row[6] #Função de Confiança ou Cargo em Comissão 
+        christmas_grati = row[7] #Gratificação Natalina
+        terco_ferias = row[8] #Férias (1/3 constitucional)
+        abono_permanencia = row[9] #Abono de Permanência
+        temp_remu = row[10] #Outras Remunerações Temporárias
+        idemnity = row[11] #Verbas Indenizatórias
+        total = row[12] #Total de rendimentos brutos 
+        prev_contrib = row[13] #Contribuição Previdenciária
+        imposto_renda = row[14] #Imposto de Renda
+        ceil_ret = row[15] #Retenção do Teto       
+
+        employees[reg] = {
+        'reg': reg,
+        'name': name,
+        'role': role,
+        'type': 'membro',
+        'workplace': workplace,
+        'active': True,
+        "income":
+        {
+            'total': total,
+            'wage': remuneration + other_verbs,
+            'perks':{
+                'total': idemnity,
+            },
+            'other':
+            { 
+                'total': trust_pos + christmas_grati + terco_ferias + abono_permanencia + temp_remu,
+                'trust_position': trust_pos,
+                'eventual_benefits': temp_remu,
+                'others_total': christmas_grati + terco_ferias + abono_permanencia,
+                'others': {
+                    'Gratificação Natalina': christmas_grati,
+                    'Férias (1/3 constitucional)': terco_ferias,
+                    'Abono de permanência': abono_permanencia,
+                }
+            },
+
+        },
+        'discounts':
+        {
+            'total': round(prev_contrib + ceil_ret + imposto_renda, 2),
+            'prev_contribution': prev_contrib,
+            'ceil_retention': ceil_ret,
+            'income_tax': imposto_renda
+        }
+    }
+    
+    return employees
+
+def employees_idemnity(file_path, employees):
+    data = read(file_path)
+
+    #Ajustando dataframe para simplificar interação
+    data = data[data['Unnamed: 4'].notna()]
+    data = data[data['Ministério Público do Estado do Espírito Santo'].notna()]
+    data = data[1:]
+    clean_currency(data, 4,7)
+
+    #Parsing Data
+    rows = data.to_numpy()
+    for row in rows:
+        reg = str(row[0]) # Matrícula
+        vacation = row[4] #ABONO  FÉR. IND. EX. ANT.
+        aux_ali = row[5] #CARTÃO ALIMENTAÇÃO
+        aux_saude = row[6] #AUXÍLIO SAÚDE
+        plantao = row[7] #Plantao
+        
+        #Há funcionários não listados na lista de remunerações mas listados na lista de indenizações
+        try:
+            emp = employees[reg]
+            exists = True
+        except:
+            exists = False
+
+        if exists :    
+
+            emp = employees[reg]
+
+            emp['income']['perks'].update({
+                'total': round( aux_ali + aux_saude, 2),
+                'food': aux_ali ,
+                'health': aux_saude,
+            })
+            emp['income']['other']['others'].update({
+                'ABONO  FÉR. IND. EX. ANT': vacation,
+                'Plantão': plantao,
+            })
+            emp['income']['other'].update({
+                'others_total': round( emp['income']['other']['others_total'] + plantao, 2),
+            })
+
+            employees[reg] = emp 
+
+    return employees
+
+def parse(file_names, year, month):
+    employees = {} 
+
+    for file_name in file_names:
+        if 'vi' not in file_name:
+            if(int(year) == 2019 and int(month) >= 8):
+                employees.update(employees_parser(file_name))
+            elif int(year) > 2019:
+                employees.update(employees_parser(file_name))
+        else:
+            employees.update(employees_idemnity(file_name, employees))
+    
+    return list(employees.values())
