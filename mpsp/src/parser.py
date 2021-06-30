43a158000b925efedf7138a08771b9a8e42a6e63
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2021-06-30 09:06:59 -0300

    Adiciona mes 05-MPSP

--- mpsp/src/parser.py
+++ mpsp/src/parser.py
@@ -215,8 +215,7 @@ def parse_active_members(file_names, month, year):
                 elif month in ["06", "07", "08", "09", "10", "11", "12"] and year == "2018":
                     employees.update(active_members_parser.parse_june_to_dec_18(fn))
                 elif year == "2021":
-                    if month in ["01", "02", "03", "04"]:
-                        
+                    if month in ["01", "02", "03", "04", "05"]:
                         employees.update(parse_employees(fn))
          
             elif "Verbas Indenizatorias" in fn:
@@ -289,7 +288,7 @@ def parse_active_members(file_names, month, year):
                                 fn, employees
                             )
                         )
-                    elif month in ["03", "04"]:
+                    elif month in ["03", "04", "05"]:
                         employees.update(
                             active_members_indemnity_parser.update_employee_indemnity_mar_apr_2021(
                                 fn, employees
