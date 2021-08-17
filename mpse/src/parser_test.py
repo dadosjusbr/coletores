dd57f40b651f79c5dbc7b79513d230ff832883dc
marcos-inja <marcosvaas@gmail.com>
2021-08-17 11:40:10 -0300

    Novos testes adicionados

--- mpse/src/parser_test.py
+++ mpse/src/parser_test.py
@@ -46,6 +46,96 @@ class TestParser(unittest.TestCase):
         self.assertDictEqual(employees[0], expected)
 
 
+    def test_membros_ativos_remuneracao_2018_5(self):
+        self.maxDiff = None
+
+        expected = {
+            "reg": "2134",
+            "name": "JOSE AUGUSTO RAMOS DA SILVA",
+            "role": "POLICIAL MILITAR ",
+            "type": "membro",
+            "workplace": "SEDE/MP",
+            "active": True,
+            "income": {
+                "total": 1259.55,
+                "wage": 1259.55,
+                "perks": {
+                    "total": 0.0,
+                    "vacation": 0.0
+                },
+                "other": {
+                    "total": 0.0,
+                    "trust_position": 0.0,
+                    "eventual_benefits": 0.0,
+                    "others_total": 0.0,
+                    "others": {
+                        "Gratificação Natalina": 0.0,
+                        "Abono de Permanência": 0.0
+                    }
+                }
+            },
+            "discounts": {
+                "total": 0.0,
+                "prev_contribution": 0.0,
+                "ceil_retention": 0.0,
+                "income_tax": 0.0
+            }
+        }
+
+        files = ['./output_test/remuneracao-membros-ativos-5-2018.ods']
+
+        employees = parser.parse(files, '2018', '5')
+
+        # Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+
+    def test_membros_ativos_remuneracao_2018_10(self):
+        self.maxDiff = None
+
+        expected = {
+            "reg": "2155",
+            "name": "LARISSA CAROLAINE MENEZES DE OLIVEIRA",
+            "role": "ASSESSOR OPERACIONAL",
+            "type": "membro",
+            "workplace": "SEDE/MP",
+            "active": True,
+            "income": {
+                "total": 3091.82,
+                "wage": 0.0,
+                "perks": {
+                    "total": 1020.9,
+                    "vacation": 0.0
+                },
+                "other": {
+                    "total": 2070.92,
+                    "trust_position": 2070.92,
+                    "eventual_benefits": 0.0,
+                    "others_total": 0.0,
+                    "others": {
+                        "Gratificação Natalina": 0.0,
+                        "Abono de Permanência": 0.0
+                    }
+                }
+            },
+            "discounts": {
+                "total": 186.38,
+                "prev_contribution": 186.38,
+                "ceil_retention": 0.0,
+                "income_tax": 0.0
+            }
+        }
+
+        files = ['./output_test/remuneracao-membros-ativos-10-2018.ods']
+
+        employees = parser.parse(files, '2018', '10')
+
+        # Verificações
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+
     def test_membros_ativos_remuneracao_2019(self):
         self.maxDiff = None
 
@@ -88,6 +178,72 @@ class TestParser(unittest.TestCase):
         # Verificações
         self.assertEqual(1, len(employees))
         self.assertDictEqual(employees[0], expected)
+
+
+    def test_membros_ativos_remuneracao_2019_8(self):
+            self.maxDiff = None
+
+            expected = {
+                "reg": "1480",
+                "name": "THIAGO LIMA FEITOZA",
+                "role": "ANALISTA DO MP -Informática - G. e A.de P. de Sistemas",
+                "type": "membro",
+                "workplace": "DIRETORIA DE TECNOLOGIA DA INFORMACAO",
+                "active": True,
+                "income": {
+                    "total": 14435.46,
+                    "wage": 9132.61,
+                    "perks": {
+                        "total": 1735.39,
+                        "vacation": 0.0,
+                        "food": 1020.9,
+                        "health": 714.49
+                    },
+                    "other": {
+                        "total": 3567.46,
+                        "trust_position": 3567.46,
+                        "eventual_benefits": 0.0,
+                        "others_total": 0.0,
+                        "others": {
+                            "Gratificação Natalina": 0.0,
+                            "Abono de Permanência": 0.0,
+                            "Auxilio Interiorização": 0.0,
+                            "Auxilio lei 8.625/93": 0.0,
+                            "Indenizações Férias/Licença-Prêmio": 0.0,
+                            "Abono Pecuniário": 0.0,
+                            "Ressarcimentos": 0.0,
+                            "GEO": 0.0,
+                            "Insalubridade": 0.0,
+                            "Periculosidade": 0.0,
+                            "Adicional Trabalho Tecnico": 0.0,
+                            "Grat. Atividade Ensino": 0.0,
+                            "Substituições": 0.0,
+                            "Cumulação": 0.0,
+                            "Represetação de Direção": 0.0,
+                            "Grat. Turma Recursal": 0.0,
+                            "Grat. Dificil Provimento": 0.0,
+                            "Grat. Acessor": 0.0,
+                            "Representação GAEGO": 0.0
+                        }
+                    }
+                },
+                "discounts": {
+                    "total": 3483.91,
+                    "prev_contribution": 1187.24,
+                    "ceil_retention": 0.0,
+                    "income_tax": 2296.67
+                }
+            }
+
+            files = ['./output_test/indenizacao-membros-ativos-8-2019.ods',
+                    './output_test/remuneracao-membros-ativos-8-2019.odt']
+
+            employees = parser.parse(files, '2019', '8')
+
+            # Verificações
+            self.assertEqual(1, len(employees))
+            self.assertDictEqual(employees[0], expected)
+
 
 
     def test_membros_ativos_remuneracao_2021_1(self):
