fa2c279c7a560c8252062dba020b3e36d5be3c81
marcos-inja <marcosvaas@gmail.com>
2021-09-14 09:39:58 -0300

    Crawler feito

--- mpma/src/crawler.py
+++ mpma/src/crawler.py
@@ -1,127 +1,100 @@
 import pathlib
 import os
 import sys
-import time
+from time import sleep
 import shutil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 
-BASE_URL = 'https://servicos-portal.mpro.mp.br/web/mp-transparente/contracheque'
-BASE_URL_MEMBROS_ATIVOS = 'https://servicos-portal.mpro.mp.br/plcVis/frameset?__report=..%2FROOT%2Frel%2Fcontracheque%2Fmembros%2FremuneracaoMembrosAtivos.rptdesign&anomes='
-BASE_URL_VERBAS_INDENIZATORIAS = 'https://servicos-portal.mpro.mp.br/plcVis/frameset?__report=..%2FROOT%2Frel%2Fcontracheque%2Fmembros%2FverbasIndenizatoriasMembrosAtivos.rptdesign&anomes='
-FLAG = ['remuneracao','verbas-indenizatorias']
-REMUNERACAO = 'remuneracao'
-VERBAS_INDENIZATORIAS = 'verbas-indenizatorias'
+class Crawler:
+    BASE_URL = 'https://folha.mpma.mp.br/transparencia/membrosativos/'
+    REMUNERACAO = 'remuneracao'
+    VERBAS_INDENIZATORIAS = 'verbas-indenizatorias'
 
-def crawl(month, year, driver_path, output_path):
-    files = []
-    
-    pathlib.Path(output_path).mkdir(exist_ok=True)
-    driver = setup_driver(driver_path, output_path)
-    driver.get(BASE_URL)
-    time.sleep(4)
-    for flag in FLAG: 
-        file_path = download(str(month), year, output_path, driver, flag)
-        files.append(file_path)
-    driver.quit()
-    return files
+    def __init__(self, month, year, driver_path, output_path):
+        self.month = month
+        self.year = year
+        self.driver_path = driver_path
+        self.output_path = output_path
+
+    def crawl(self):
+        pathlib.Path(self.output_path).mkdir(exist_ok=True)
+        driver = self.setup_driver(self.driver_path, self.output_path)
+        # Faz duas chamadas uma para pegar a remuneração e outra para pegar as verbas indenizatorias
+        # Carrega a pagina de remuneração
+        driver.get(self.BASE_URL)
+        sleep(5)
+        self.select_month_and_year(self.month, self.year, driver)
+        file_path = self.download(self.month, self.year, self.output_path, driver)
+
+        driver.quit()
+        return file_path
 
 
-def download(month, year, output_path, driver, flag):  
-    driver.get(BASE_URL)
-    main_tab = driver.window_handles[0]
-    time.sleep(5)
+    def select_month_and_year(self, month, year, driver):
+        # Seleciona o ano
+        current_year = driver.find_element(By.XPATH, '//*[@id="selAno"]')
+        # Esse Select server para pegar as tags select, onde posso passar o valor diretamente,
+        # para pegar qual opção o usuario quer.
+        select_year = Select(current_year)
+        select_year.select_by_value(year)
+        sleep(5)
 
-    if(flag == REMUNERACAO):
-        document_type = driver.find_element(By.XPATH, '//*[@id="article_10154_29101_2483282_1.3"]/p/span/a')
-        document_type.click()
-    else:
-        document_type = driver.find_element(By.XPATH, '//*[@id="article_10154_29101_2483282_1.3"]/p/span/span/span/span/span/a')
-        document_type.click()   
+        current_month = driver.find_element(By.XPATH, '//*[@id="selMes"]')
+        select_month = Select(current_month)
+        select_month.select_by_value(month)
+        sleep(2)
 
-    time.sleep(3)
-    select_year = driver.find_element(By.XPATH, '//*[@id="selectAno"]')
-    select_year.click()
-    if(year in ['2018', '2019', '2020']):
-        if(year == "2018"):
-            select_year = driver.find_element(By.XPATH, '//*[@id="selectAno"]/option[4]')
-        elif(year == "2019"):
-            select_year = driver.find_element(By.XPATH, '//*[@id="selectAno"]/option[3]')
-        elif(year == "2020"):
-            select_year = driver.find_element(By.XPATH, '//*[@id="selectAno"]/option[2]')
+        button_enviar = driver.find_element(By.XPATH, '//*[@id="btOk"]')
+        button_enviar.click()
+        sleep(25)
 
-        select_year.click()
-        
-    time.sleep(1)
-    x_path = '//*[@id="selectMes"]/option[' + month + ']'
-    current_month = driver.find_element(By.XPATH, x_path)
-    current_month.click()
-    time.sleep(1)
-    new_url = ''
 
-    # Downloading the file
-    if(flag == REMUNERACAO):
-        show_data = driver.find_element(By.XPATH, '//*[@id="article_10154_29101_2483760_1.9"]/table/tbody/tr[4]/td[1]/input')
-        show_data.click()
-        new_url = BASE_URL_MEMBROS_ATIVOS + year + month + '&nome=&cargo=&lotacao='
+    def download(self, month, year, output_path, driver):
+        files = []
+        # Baixa e dar um nome para as remunerações
+        select_div_remuneration = driver.find_element(By.XPATH, '//*[@id="topo"]/p/a')
+        select_div_remuneration.click()
+        sleep(5)
+        name_file_remuneration = self.format_filename(month, year, output_path, self.REMUNERACAO)
+        files.append(name_file_remuneration)
 
-    elif(flag == VERBAS_INDENIZATORIAS):
-        show_data = driver.find_element(By.XPATH, '//*[@id="article_10154_29101_5313882_1.3"]/table/tbody/tr[4]/td[1]')
-        show_data.click()
-        new_url = BASE_URL_VERBAS_INDENIZATORIAS + year + month
+        # Baixa e dar um nome para as verbas indenizatórias
+        select_div_indemnization = driver.find_element(By.XPATH, '//*[@id="divremuneracoestemporarias"]/p/a')
+        select_div_indemnization.click()
+        sleep(5)
+        name_file_indemnization = self.format_filename(month, year, output_path, self.VERBAS_INDENIZATORIAS)
+        files.append(name_file_indemnization)
 
-    new_tab = driver.window_handles[1]
-    time.sleep(2)
-    driver.get(new_url)
-    time.sleep(12)
-    export = driver.find_element(By.XPATH, '//*[@id="toolbar"]/table/tbody/tr[2]/td[6]/input')
-    export.click()
-    time.sleep(4)
-    select_columns = driver.find_element(By.XPATH, '//*[@id="simpleExportDialogBody"]/tbody/tr[5]/td[2]/table/tbody/tr/td/table/tbody/tr[1]/td')
-    select_columns.click()
-    download = driver.find_element(By.XPATH, '//*[@id="simpleExportDataDialogokButton"]')
-    download.click()
+        return files
 
-    # Formating the filename
-    time.sleep(5)
-    file_name = format_filename(output_path, month, year, flag)
-    time.sleep(3)
+    # Faz a configuração do driver do navegador google chrome
+    def setup_driver(self, driver_path, output_path):
+        # Seting the directorys to be used by selenium
+        current_directory = os.getcwd()
+        path_chrome = current_directory + driver_path
+        # Attributing the paths to the webdriver
+        prefs = {"download.default_directory": output_path}
+        chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        chrome_options.add_experimental_option("prefs", prefs)
+        return webdriver.Chrome(executable_path=path_chrome, chrome_options=chrome_options)
 
-    # Closing new tabs
-    driver.switch_to_window(new_tab)
-    driver.close()
-    driver.switch_to_window(main_tab)
 
-    return file_name
+    def format_filename(self, month, year, output_path, flag):
+        # Identifying the name of the last downloaded file
+        filename = max([os.path.join(output_path, f) for f in os.listdir(output_path)], key=os.path.getctime)
+        new_filename = ''
 
-def setup_driver(driver_path, output_path):
-    # Seting the directorys to be used by selenium
-    current_directory = os.getcwd()
-    path_chrome = current_directory + driver_path
-    path_prefs = output_path
+        # renaming the file properly, according to the month
+        if(flag == self.REMUNERACAO):
+            new_filename = month + "-" + year + "-" + flag +"-membros-ativos" + ".xls"
+        elif(flag == self.VERBAS_INDENIZATORIAS):
+            new_filename = month + "-" + year + "-" + flag + "-membros-ativos" + ".xls"
 
-    # Attributing the paths to the webdriver
-    prefs = {"download.default_directory" : path_prefs}
-    chrome_options = webdriver.ChromeOptions()
-
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-setuid-sandbox')
-    chrome_options.add_experimental_option("prefs", prefs)
-    return webdriver.Chrome(executable_path = path_chrome, chrome_options = chrome_options)
-
-def format_filename(output_path, month, year, flag):
-    # Identifying the name of the last downloaded file
-    filename = max([os.path.join(output_path, f) for f in os.listdir(output_path)], key=os.path.getctime)
-
-    # renaming the file properly, according to the month
-    if(flag == REMUNERACAO):
-        new_filename = month + "-" + year + "-" +flag +"-membros-ativos" + ".csv"
-    elif(flag == VERBAS_INDENIZATORIAS):
-        new_filename = month + "-" + year + "-" +flag + "-membros-ativos" + ".csv"
-
-    shutil.move(filename,os.path.join(output_path,r"{}".format(new_filename)))
-    new_output_path = output_path + "/" + new_filename
-
-    return new_output_path 
+        shutil.move(filename,os.path.join(output_path,r"{}".format(new_filename)))
+        new_output_path = output_path + "/" + new_filename
+        return new_output_path 
---
+++ mpma/src/main.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from crawler import Crawler
+import json
+import datetime
+
+if('MONTH' in os.environ):
+    month = os.environ['MONTH']
+    month = month.zfill(2)
+else:
+    sys.stderr.write("Invalid arguments, missing parameter: 'MONTH'.\n")
+    os._exit(1)
+if('YEAR' in os.environ):
+    year = os.environ['YEAR']
+else:
+    sys.stderr.write("Invalid arguments, missing parameter: 'YEAR'.\n")
+    os._exit(1)
+if('DRIVER_PATH' in os.environ):
+    driver_path = os.environ['DRIVER_PATH']
+else:
+    sys.stderr.write("Invalid arguments, missing parameter: 'DRIVER_PATH'.\n")
+    os._exit(1)
+if('OUTPUT_FOLDER' in os.environ):
+    output_path = os.environ['OUTPUT_FOLDER']
+else:
+    output_path = "/output"
+if('GIT_COMMIT' in os.environ):
+    crawler_version = os.environ['GIT_COMMIT']
+else:
+    sys.stderr.write("crawler_version cannot be empty")
+    os._exit(1)
+
+now = datetime.datetime.now()
+current_year = now.year
+current_month = now.month
+
+def main():
+    file_names = Crawler(month, year, driver_path, output_path).crawl()
+    print(file_names)
+    
+if __name__ == '__main__':
+    main()
