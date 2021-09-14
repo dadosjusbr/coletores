55958e40cd38298d0e31f5207b18e8ccf9d4c3cd
marcos-inja <marcosvaas@gmail.com>
2021-09-14 10:21:24 -0300

    base parse

--- mpma/src/parser.py
+++ mpma/src/parser.py
@@ -1,4 +1,19 @@
 import pandas as pd
+import sys, os
+from parser_remuneration import Remuneration
+from update_remuneration import UpdateRemuneration
+
+def openCsv(file):
+    try:
+        data = pd.read_csv(file)
+        return data
+    except Exception as excep:
+        sys.stderr.write(f"Não foi possível ler o arquivo: {file}. O seguinte erro foi gerado: {str(excep)}")
+        os._exit(1)
 
 def parse(data):
-    
+    remuneration = openCsv(data[0])
+    indemnization = openCsv(data[1])
+    employes_remuneration = Remuneration(remuneration).parser()
+    employes_update = UpdateRemuneration(employes_remuneration, indemnization).update()
+    return list(employes_update.values())
