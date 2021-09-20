a845b9b1d2a8774837217758e339df009045052a
marcos-inja <marcosvaas@gmail.com>
2021-09-20 11:42:54 -0300

    feat: main inicial, chamando apenas o crawler

---
+++ mpmt/src/main.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import crawler
+import datetime
+
+if('MONTH' in os.environ):
+    month = os.environ['MONTH']
+    month = month.zfill(2)
+else:
+    sys.stderr.write("Invalid arguments, missing parameter: 'MONTH'.\n")
+    os._exit(1)
+if('YEAR' in os.environ):
+    year = os.environ['YEAR']
+else:
+    sys.stderr.write("Invalid arguments, missing parameter: 'YEAR'.\n")
+    os._exit(1)
+if('OUTPUT_FOLDER' in os.environ):
+    output_path = os.environ['OUTPUT_FOLDER']
+else:
+    output_path = "/output"
+if('GIT_COMMIT' in os.environ):
+    crawler_version = os.environ['GIT_COMMIT']
+else:
+    sys.stderr.write("crawler_version cannot be empty")
+    os._exit(1)
+
+now = datetime.datetime.now()
+current_year = now.year
+current_month = now.month
+
+def main():
+    crawler.crawl(year, month, output_path)
+    
+if __name__ == '__main__':
+    main()
